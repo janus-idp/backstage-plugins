samuelcguimaraes/backstage

Bump commander to version 9.1.0
Signed-off-by: Fredrik Adelöw <freben@gmail.com>

packages/cli/src/commands/versions/lint.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { Command } from 'commander';
+import { OptionValues } from 'commander';
 import { Lockfile } from '../../lib/versioning';
 import { paths } from '../../lib/paths';
 import partition from 'lodash/partition';
@@ -43,7 +43,7 @@ export const forbiddenDuplicatesFilter = (name: string) =>
   FORBID_DUPLICATES.some(pattern => pattern.test(name)) &&
   !ALLOW_DUPLICATES.some(pattern => pattern.test(name));
 
-export default async (cmd: Command) => {
+export default async (cmd: CommOptionValuesand) => {
   const fix = Boolean(cmd.fix);
 
   let success = true;

janus-idp/backstage-plugins

packages/cli/src/commands/versions/lint.ts
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-import { Command } from 'commander';
+import { OptionValues } from 'commander';
 import { Lockfile } from '../../lib/versioning';
 import { paths } from '../../lib/paths';
 import partition from 'lodash/partition';
@@ -47,7 +47,7 @@ export const forbiddenDuplicatesFilter = (name: string) =>
   FORBID_DUPLICATES.some(pattern => pattern.test(name)) &&
   !ALLOW_DUPLICATES.some(pattern => pattern.test(name));
 
-export default async (cmd: Command) => {
+export default async (cmd: OptionValues) => {
   const fix = Boolean(cmd.fix);
 
   let success = true;