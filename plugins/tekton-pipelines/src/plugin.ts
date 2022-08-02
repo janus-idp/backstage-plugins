5d182af422e6023a3894e38d1e878010c431b597
Tihomir Yordanov <ty@jquad.de>
2022-08-02 16:18:18 +0200

    added license file

--- plugins/tekton-pipelines/src/plugin.ts
+++ plugins/tekton-pipelines/src/plugin.ts
@@ -21,7 +21,7 @@ import { Entity } from '@backstage/catalog-model';
 
 export const TEKTON_PIPELINES_BUILD_NAMESPACE = 'tektonci/build-namespace';
 
-export const isTektonCiAvailable = (entity: Entity) =>Boolean(entity?.metadata.annotations?.[TEKTON_PIPELINES_BUILD_NAMESPACE]);
+export const isTektonCiAvailable = (entity: Entity) => Boolean(entity?.metadata.annotations?.[TEKTON_PIPELINES_BUILD_NAMESPACE]);
 
 export const tektonPipelinesPluginPlugin = createPlugin({
   id: 'tekton-pipelines-plugin',
