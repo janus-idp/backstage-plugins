4d3533a0d9e67ac04f5a253fdb40ccc727c9fa27
Samuel Kopecký <skopecky@redhat.com>
2022-11-09 22:36:10 +0100

    chore: Remove unsued function (#188)
    Signed-off-by: SamoKopecky <skopecky@redhat.com>
    
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

--- plugins/rhacm-backend/src/helpers/config.ts
+++ plugins/rhacm-backend/src/helpers/config.ts
@@ -3,15 +3,6 @@ import { Config } from '@backstage/config';
 const CLUSTERS_PATH = 'kubernetes.clusterLocatorMethods';
 const HUB_CLUSTER_CONFIG_PATH = 'rhacm.hub';
 
-export const getClustersFromConfig = (config: Config): Config[] => {
-  const clusters = config
-    .getConfigArray(CLUSTERS_PATH)
-    .flatMap(v =>
-      v.getString('type') !== 'config' ? [] : v.getConfigArray('clusters'),
-    );
-  return clusters;
-};
-
 export const getHubClusterFromConfig = (config: Config) => {
   try {
     config.getString(HUB_CLUSTER_CONFIG_PATH)
