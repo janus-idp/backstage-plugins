e71d8df08c772a16af0b855c4548daf06a83f0c6
Nikolay <nk@jquad.de>
2022-11-10 20:34:39 +0100

    fixed Router path

--- plugins/tekton-pipelines/src/hooks/usePipelineRunObjects.ts
+++ plugins/tekton-pipelines/src/hooks/usePipelineRunObjects.ts
@@ -4,6 +4,7 @@ import { tektonApiRef } from '../api/types';
 import { PipelineRun, PipelineRunsByEntityRequest } from '@jquad-group/plugin-tekton-pipelines-common';
 import useAsync from 'react-use/lib/useAsync';
 import { Entity } from '@backstage/catalog-model';
+import logger from '../logging/logger';
 
 export interface PipelineRunObjects {
   pipelineRunObjects?: PipelineRun[];
@@ -19,6 +20,7 @@ export const usePipelineRunObjects = (
   const request: PipelineRunsByEntityRequest = {
     entity: entity,
   }
+
   const getObjects = useCallback(async (): Promise<PipelineRun[]> => {
     return await tektonApi.getPipelineRuns(request,"","","","","");
     // eslint-disable-next-line react-hooks/exhaustive-deps
