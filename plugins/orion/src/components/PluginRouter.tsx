9c6710414e39fdbd31021036ef2ec98e82d9f49c
Marek Libra <marek.libra@gmail.com>
2023-02-22 10:25:13 +0100

    Introduce in-plugin tab navigation
    So far it is a combination of Backstage's tabs and react-router.
    We might change this to use just the tabs for smoother UX.

--- plugins/orion/src/components/PluginRouter.tsx
+++ plugins/orion/src/components/PluginRouter.tsx
@@ -1,20 +1,39 @@
 import React from 'react';
-import {Route, Routes} from 'react-router-dom';
-import {ProjectOverviewPage} from './projectOverview';
-import {Workflow} from "./workflow";
-import {Deploy} from "./deploy";
-import {Notification} from "./notification";
-import {Training} from "./training";
-import {Metrics} from "./metrics";
+import { Route, Routes } from 'react-router-dom';
+import { ProjectOverviewPage } from './projectOverview';
+import { Workflow } from './workflow';
+import { Deploy } from './deploy';
+import { Notification } from './notification';
+import { Training } from './training';
+import { Metrics } from './metrics';
+import {
+  AssessmentIcon,
+  NotificationsIcon,
+  ProjectsIcon,
+  TrainingIcon,
+} from './icons';
+
+export const pluginRoutePrefix = '/parodos';
+
+export const navigationMap = [
+  { label: 'Projects', route: '/project-overview', icon: <ProjectsIcon /> },
+  { label: 'Assessment', route: '/workflow', icon: <AssessmentIcon /> },
+  {
+    label: 'Notifications',
+    route: '/notification',
+    icon: <NotificationsIcon />,
+  },
+  { label: 'Training', route: '/training', icon: <TrainingIcon /> },
+];
 
 export const PluginRouter = () => (
-    <Routes>
-        <Route path="/" element={<ProjectOverviewPage/>}/>
-        <Route path="/project-overview" element={<ProjectOverviewPage/>}/>
-        <Route path="/workflow" element={<Workflow/>}/>
-        <Route path="/deploy" element={<Deploy/>}/>
-        <Route path="/notification" element={<Notification/>}/>
-        <Route path="/training" element={<Training/>}/>
-        <Route path="/metrics" element={<Metrics/>}/>
-    </Routes>
+  <Routes>
+    <Route path="/" element={<ProjectOverviewPage />} />
+    <Route path="/project-overview" element={<ProjectOverviewPage />} />
+    <Route path="/workflow" element={<Workflow />} />
+    <Route path="/deploy" element={<Deploy />} />
+    <Route path="/notification" element={<Notification />} />
+    <Route path="/training" element={<Training />} />
+    <Route path="/metrics" element={<Metrics />} />
+  </Routes>
 );
--- plugins/orion/src/components/projectOverview/ProjectOverview.tsx
+++ plugins/orion/src/components/projectOverview/ProjectOverview.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import {
-  Content,
   ContentHeader,
   Link,
-  Page,
   Select,
   SupportButton,
 } from '@backstage/core-components';
@@ -11,7 +9,7 @@ import { Add } from '@material-ui/icons';
 import { Grid } from '@material-ui/core';
 import { EmptyProjectsState } from './EmptyProjectsState';
 import { useCommonStyles } from '../../styles';
-import { PageHeader } from '../PageHeader';
+import { ParodosPage } from '../ParodosPage';
 
 export const ProjectOverviewPage = () => {
   const commonStyles = useCommonStyles();
@@ -31,37 +29,34 @@ export const ProjectOverviewPage = () => {
   };
 
   return (
-    <Page themeId="tool">
-      <PageHeader />
-      <Content>
-        <ContentHeader title="Projects overview">
-          <SupportButton title="Need help?">Lorem Ipsum</SupportButton>
-        </ContentHeader>
+    <ParodosPage>
+      <ContentHeader title="Projects overview">
+        <SupportButton title="Need help?">Lorem Ipsum</SupportButton>
+      </ContentHeader>
 
-        <Grid container direction="row">
-          <Grid item xs={3}>
-            <Select
-              onChange={onFilterProjects}
-              placeholder="All projects"
-              label="Filter by"
-              items={projectFilterItems}
-            />
-          </Grid>
-
-          <Grid item xs={3}>
-            <Link to="/parodos/workflow">
-              <Add className={commonStyles.inlineicon} />
-              &nbsp;Add new project
-            </Link>
-          </Grid>
-
-          {!projects?.length && (
-            <Grid item xs={12}>
-              <EmptyProjectsState />
-            </Grid>
-          )}
+      <Grid container direction="row">
+        <Grid item xs={3}>
+          <Select
+            onChange={onFilterProjects}
+            placeholder="All projects"
+            label="Filter by"
+            items={projectFilterItems}
+          />
         </Grid>
-      </Content>
-    </Page>
+
+        <Grid item xs={3}>
+          <Link to="/parodos/workflow">
+            <Add className={commonStyles.inlineicon} />
+            &nbsp;Add new project
+          </Link>
+        </Grid>
+
+        {!projects?.length && (
+          <Grid item xs={12}>
+            <EmptyProjectsState />
+          </Grid>
+        )}
+      </Grid>
+    </ParodosPage>
   );
 };
