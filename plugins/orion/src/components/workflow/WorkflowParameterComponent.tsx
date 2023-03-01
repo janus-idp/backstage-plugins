ff00badf9fcca7c11011b3768c528478dc9a4090
Marek Libra <marek.libra@gmail.com>
2023-03-01 15:34:59 +0100

    FLPATH-53: Add validation of workflow params

--- plugins/orion/src/components/workflow/WorkflowParameterComponent.tsx
+++ plugins/orion/src/components/workflow/WorkflowParameterComponent.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import { TextField } from '@material-ui/core';
+import { makeStyles, TextField } from '@material-ui/core';
 import { DatePicker } from '@patternfly/react-core';
 import { WorkFlowTaskParameterType } from '../types';
 import { Select, SelectedItems, SelectItem } from '@backstage/core-components';
 import { WorkflowParametersContext } from '../../context/WorkflowParametersContext';
+import { getParamValidator, ValidationType } from './validators';
+
+const useStyles = makeStyles({
+  validationError: {
+    color: 'red',
+  },
+});
+
+const ValidationError: React.FC<{ validation: ValidationType }> = ({
+  validation,
+}) => {
+  const styles = useStyles();
+  return <div className={styles.validationError}>{validation}</div>;
+};
 
 export const WorkflowParameterComponent: React.FC<{
   param: WorkFlowTaskParameterType;
 }> = ({ param }) => {
-  const { getParamValue, setParamValue } = React.useContext(
+  const { getParamValue, setParamValue, getParamValidation } = React.useContext(
     WorkflowParametersContext,
   );
+
+  const validation = getParamValidation(param.key);
 
   if (['TEXT', 'PASSWORD', 'EMAIL', 'URL', 'NUMBER'].includes(param.type)) {
     let type: string;
@@ -34,14 +50,23 @@ export const WorkflowParameterComponent: React.FC<{
     return (
       <TextField
         id={param.key}
-        helperText={param.description}
+        helperText={
+          validation ? (
+            <ValidationError validation={validation} />
+          ) : (
+            param.description
+          )
+        }
         label={param.key}
         required={!param.optional}
         type={type}
         value={getParamValue(param.key)}
         onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-          // TODO: add validation
-          setParamValue(param.key, event.target.value);
+          setParamValue(
+            param.key,
+            event.target.value,
+            getParamValidator(param),
+          );
         }}
         variant="outlined"
       />
@@ -53,11 +78,16 @@ export const WorkflowParameterComponent: React.FC<{
       <DatePicker
         label={param.key}
         required={!param.optional}
-        helperText={param.description}
+        helperText={
+          validation ? (
+            <ValidationError validation={validation} />
+          ) : (
+            param.description
+          )
+        }
         value={getParamValue(param.key)}
         onChange={(_, value: string) => {
-          // TODO: add validation
-          setParamValue(param.key, value);
+          setParamValue(param.key, value, getParamValidator(param));
         }}
       />
     );
@@ -73,11 +103,12 @@ export const WorkflowParameterComponent: React.FC<{
       <Select
         onChange={(selected: SelectedItems) => {
           const value = Array.isArray(selected) ? selected[0] : selected;
-          setParamValue(param.key, value as string);
+          setParamValue(param.key, value as string, getParamValidator(param));
         }}
         label={param.key}
-        // required={!param.optional}
-        // helperText={param.description}
+        // TODO: required={!param.optional}
+        // TODO: helperText={param.description}
+        // TODO: show validation
         items={items}
         selected={getParamValue(param.key)}
       />
