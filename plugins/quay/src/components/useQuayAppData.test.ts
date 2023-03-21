b001f1eb2af0ba66ae4d7a36fa40036e5569ebc9
SamoKopecky <skopecky@redhat.com>
2023-03-21 12:54:24 +0100

    feat(quay): Add tests
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

---
+++ plugins/quay/src/components/useQuayAppData.test.ts
@@ -0,0 +1,33 @@
+import { useQuayAppData } from './useQuayAppData';
+import { Entity } from '@backstage/catalog-model';
+
+describe('useQuayAppData', () => {
+  it('should correctly get the repository flag from the entity', () => {
+    const entity: Entity = {
+      apiVersion: 'backstage.io/v1alpha1',
+      kind: 'Component',
+      metadata: {
+        name: 'foo',
+        annotations: { 'quay.io/repository-slug': 'foo/bar' },
+      },
+    };
+
+    const result = useQuayAppData({ entity });
+
+    expect(result).toEqual({ repositorySlug: 'foo/bar' });
+  });
+
+  it('should throw an error when the annotation is not present', () => {
+    const entity: Entity = {
+      apiVersion: 'backstage.io/v1alpha1',
+      kind: 'Component',
+      metadata: {
+        name: 'foo',
+      },
+    };
+
+    const useResult = () => useQuayAppData({ entity });
+
+    expect(useResult).toThrow("'Quay' annotations are missing");
+  });
+});
