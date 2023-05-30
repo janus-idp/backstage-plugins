c77a10ff80be3635a985ce459c045f56e2c8433b
Samuel Kopecký <skopecky@redhat.com>
2023-05-30 20:32:21 +0200

    feat(ocm)!: remake OCM UX (#386)
    Convert `ClusterStatusPage` to a table
    Remove `ClusterStatusCard`
    Remove `ClusterAllocatableResourceCard`
    Remake `ClusterInfoCard`
    
    Signed-off-by: SamoKopecky <skopecky@redhat.com>

--- plugins/ocm/src/components/ClusterInfoCard/ClusterInfoCard.tsx
+++ plugins/ocm/src/components/ClusterInfoCard/ClusterInfoCard.tsx
@@ -1,62 +1,27 @@
 import React from 'react';
-import { Button, Grid, makeStyles, Tooltip } from '@material-ui/core';
-import ArrowUpwardRoundedIcon from '@material-ui/icons/ArrowUpwardRounded';
 import { useCluster } from '../ClusterContext';
 import { TableCardFromData } from '../TableCardFromData';
 
-const useStyles = makeStyles({
-  button: {
-    textTransform: 'none',
-    borderRadius: 16,
-    margin: '0px',
-    paddingLeft: '4px',
-    paddingRight: '4px',
-  },
-});
+import { Status, Update } from '../common';
 
 export const ClusterInfoCard = () => {
   const { data } = useCluster();
-  const classes = useStyles();
 
   if (!data) {
     return null;
   }
 
   data.openshiftVersion = (
-    <>
-      {data.update?.available ? (
-        <Grid container direction="column" spacing={0}>
-          <Grid item>{data.openshiftVersion}</Grid>
-          <Grid item>
-            <Tooltip title={`Version ${data.update?.version!} available`}>
-              <Button
-                variant="text"
-                color="primary"
-                startIcon={<ArrowUpwardRoundedIcon style={{ fontSize: 22 }} />}
-                className={classes.button}
-                href={data.update?.url}
-                size="small"
-              >
-                Upgrade available
-              </Button>
-            </Tooltip>
-          </Grid>
-        </Grid>
-      ) : (
-        data.openshiftVersion
-      )}
-    </>
+    <Update data={{ version: data.openshiftVersion!, update: data.update! }} />
   ) as any;
-
+  data.status = (<Status status={data.status} />) as any;
   const nameMap = new Map<string, string>([
     ['name', 'Name'],
+    ['status', 'Status'],
     ['kubernetesVersion', 'Kubernetes version'],
     ['openshiftId', 'OpenShift ID'],
     ['openshiftVersion', 'OpenShift version'],
     ['platform', 'Platform'],
-    ['region', 'Region'],
-    ['consoleUrl', 'Console URL'],
-    ['oauthUrl', 'OAuth URL'],
   ]);
   return (
     <TableCardFromData data={data} title="Cluster Info" nameMap={nameMap} />
--- plugins/ocm/src/components/ClusterStatusCard/index.ts
+++
@@ -1,1 +0,0 @@
-export { ClusterStatusCard } from './ClusterStatusCard';
