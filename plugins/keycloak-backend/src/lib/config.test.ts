ac780b64ee7b266749d7d088ca0e61938b31e9a2
Paul Schultz <pschultz@pobox.com>
2023-06-08 10:15:52 -0500

    remove mocking in config.test.ts
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- plugins/keycloak-backend/src/lib/config.test.ts
+++ plugins/keycloak-backend/src/lib/config.test.ts
@@ -1,29 +1,32 @@
-import * as backendTasks from '@backstage/backend-tasks';
-import { Config } from '@backstage/config';
+import { ConfigReader } from '@backstage/config';
+
+import deepmerge from 'deepmerge';
 
 import { readProviderConfigs } from './config';
 
-jest
-  .spyOn(backendTasks, 'readTaskScheduleDefinitionFromConfig')
-  .mockReturnValue({
-    frequency: { hours: 1 },
-    timeout: { minutes: 50 },
-    initialDelay: { seconds: 15 },
-  } as backendTasks.TaskScheduleDefinition);
+// jest
+//   .spyOn(backendTasks, 'readTaskScheduleDefinitionFromConfig')
+//   .mockReturnValue({
+//     frequency: { hours: 1 },
+//     timeout: { minutes: 50 },
+//     initialDelay: { seconds: 15 },
+//   } as backendTasks.TaskScheduleDefinition);
 
-const exampleProviderConfigInstance = {
-  has: jest.fn().mockReturnValueOnce(false),
-  getConfig: jest.fn().mockReturnValueOnce(undefined),
-  getString: jest.fn().mockReturnValueOnce('http://localhost:8080/auth'),
-  getOptionalString: jest.fn().mockReturnValue(undefined),
-  getOptionalNumber: jest.fn().mockReturnValue(undefined),
-};
+const BASIC_VALID_CONFIG = {
+  catalog: {
+    providers: {
+      keycloakOrg: {
+        default: {
+          baseUrl: 'http://localhost:8080/auth',
+        },
+      },
+    },
+  },
+} as const;
 
 describe('readProviderConfigs', () => {
   it('should return an empty array if no providers are configured', () => {
-    const config = {
-      getOptionalConfig: jest.fn().mockReturnValue(undefined),
-    } as unknown as Config;
+    const config = new ConfigReader({});
 
     const result = readProviderConfigs(config);
 
@@ -31,12 +34,7 @@ describe('readProviderConfigs', () => {
   });
 
   it('should return an array of provider configs', () => {
-    const config = {
-      getOptionalConfig: jest.fn().mockReturnValue({
-        keys: jest.fn().mockReturnValue(['default']),
-        getConfig: jest.fn().mockReturnValue(exampleProviderConfigInstance),
-      }),
-    } as unknown as Config;
+    const config = new ConfigReader(BASIC_VALID_CONFIG);
 
     const result = readProviderConfigs(config);
 
@@ -58,34 +56,38 @@ describe('readProviderConfigs', () => {
   });
 
   it('should return an array of provider configs with optional values', () => {
-    const config = {
-      getOptionalConfig: jest.fn().mockReturnValue({
-        keys: jest.fn().mockReturnValue(['default']),
-        getConfig: jest.fn().mockReturnValue({
-          ...exampleProviderConfigInstance,
-          has: jest.fn().mockReturnValueOnce(true),
-          getOptionalString: jest
-            .fn()
-            .mockReturnValueOnce('myrealm')
-            .mockReturnValueOnce('myloginrealm')
-            .mockReturnValueOnce('myusername')
-            .mockReturnValueOnce('mypassword')
-            .mockReturnValueOnce('myclientid')
-            .mockReturnValueOnce('myclientsecret'),
-          getOptionalNumber: jest
-            .fn()
-            .mockReturnValueOnce(100)
-            .mockReturnValueOnce(200),
-        }),
+    const config = new ConfigReader(
+      deepmerge(BASIC_VALID_CONFIG, {
+        catalog: {
+          providers: {
+            keycloakOrg: {
+              default: {
+                realm: 'myrealm',
+                loginRealm: 'myloginrealm',
+                username: 'myusername',
+                password: 'mypassword',
+                clientId: 'myclientid',
+                clientSecret: 'myclientsecret',
+                userQuerySize: 100,
+                groupQuerySize: 200,
+                schedule: {
+                  frequency: { hours: 1 },
+                  timeout: { minutes: 50 },
+                  initialDelay: { seconds: 15 },
+                },
+              },
+            },
+          },
+        },
       }),
-    } as unknown as Config;
+    );
 
     const result = readProviderConfigs(config);
 
     expect(result).toEqual([
       {
         id: 'default',
-        baseUrl: undefined,
+        baseUrl: 'http://localhost:8080/auth',
         loginRealm: 'myloginrealm',
         realm: 'myrealm',
         username: 'myusername',
@@ -95,6 +97,7 @@ describe('readProviderConfigs', () => {
         userQuerySize: 100,
         groupQuerySize: 200,
         schedule: {
+          scope: undefined,
           frequency: { hours: 1 },
           timeout: { minutes: 50 },
           initialDelay: { seconds: 15 },
@@ -104,22 +107,19 @@ describe('readProviderConfigs', () => {
   });
 
   it('should throw an error if clientId is provided without clientSecret', () => {
-    const config = {
-      getOptionalConfig: jest.fn().mockReturnValue({
-        keys: jest.fn().mockReturnValue(['default']),
-        getConfig: jest.fn().mockReturnValue({
-          ...exampleProviderConfigInstance,
-          getOptionalString: jest
-            .fn()
-            .mockReturnValueOnce('myrealm')
-            .mockReturnValueOnce('myloginrealm')
-            .mockReturnValueOnce(undefined)
-            .mockReturnValueOnce(undefined)
-            .mockReturnValueOnce('myclientid')
-            .mockReturnValueOnce(undefined),
-        }),
+    const config = new ConfigReader(
+      deepmerge(BASIC_VALID_CONFIG, {
+        catalog: {
+          providers: {
+            keycloakOrg: {
+              default: {
+                clientId: 'myclientid',
+              },
+            },
+          },
+        },
       }),
-    } as unknown as Config;
+    );
 
     expect(() => readProviderConfigs(config)).toThrow(
       `clientSecret must be provided when clientId is defined.`,
@@ -127,22 +127,19 @@ describe('readProviderConfigs', () => {
   });
 
   it('should throw an error if clientSecret is provided without clientId', () => {
-    const config = {
-      getOptionalConfig: jest.fn().mockReturnValue({
-        keys: jest.fn().mockReturnValue(['default']),
-        getConfig: jest.fn().mockReturnValue({
-          ...exampleProviderConfigInstance,
-          getOptionalString: jest
-            .fn()
-            .mockReturnValueOnce('myrealm')
-            .mockReturnValueOnce('myloginrealm')
-            .mockReturnValueOnce(undefined)
-            .mockReturnValueOnce(undefined)
-            .mockReturnValueOnce(undefined)
-            .mockReturnValueOnce('myclientsecret'),
-        }),
+    const config = new ConfigReader(
+      deepmerge(BASIC_VALID_CONFIG, {
+        catalog: {
+          providers: {
+            keycloakOrg: {
+              default: {
+                clientSecret: 'myclientsecret',
+              },
+            },
+          },
+        },
       }),
-    } as unknown as Config;
+    );
 
     expect(() => readProviderConfigs(config)).toThrow(
       `clientId must be provided when clientSecret is defined.`,
@@ -150,22 +147,19 @@ describe('readProviderConfigs', () => {
   });
 
   it('should throw an error if username is provided without password', () => {
-    const config = {
-      getOptionalConfig: jest.fn().mockReturnValue({
-        keys: jest.fn().mockReturnValue(['default']),
-        getConfig: jest.fn().mockReturnValue({
-          ...exampleProviderConfigInstance,
-          getOptionalString: jest
-            .fn()
-            .mockReturnValueOnce('myrealm')
-            .mockReturnValueOnce('myloginrealm')
-            .mockReturnValueOnce('myusername')
-            .mockReturnValueOnce(undefined)
-            .mockReturnValueOnce(undefined)
-            .mockReturnValueOnce(undefined),
-        }),
+    const config = new ConfigReader(
+      deepmerge(BASIC_VALID_CONFIG, {
+        catalog: {
+          providers: {
+            keycloakOrg: {
+              default: {
+                username: 'myusername',
+              },
+            },
+          },
+        },
       }),
-    } as unknown as Config;
+    );
 
     expect(() => readProviderConfigs(config)).toThrow(
       `password must be provided when username is defined.`,
@@ -173,22 +167,19 @@ describe('readProviderConfigs', () => {
   });
 
   it('should throw an error if password is provided without username', () => {
-    const config = {
-      getOptionalConfig: jest.fn().mockReturnValue({
-        keys: jest.fn().mockReturnValue(['default']),
-        getConfig: jest.fn().mockReturnValue({
-          ...exampleProviderConfigInstance,
-          getOptionalString: jest
-            .fn()
-            .mockReturnValueOnce('myrealm')
-            .mockReturnValueOnce('myloginrealm')
-            .mockReturnValueOnce(undefined)
-            .mockReturnValueOnce('mypassword')
-            .mockReturnValueOnce(undefined)
-            .mockReturnValueOnce(undefined),
-        }),
+    const config = new ConfigReader(
+      deepmerge(BASIC_VALID_CONFIG, {
+        catalog: {
+          providers: {
+            keycloakOrg: {
+              default: {
+                password: 'mypassword',
+              },
+            },
+          },
+        },
       }),
-    } as unknown as Config;
+    );
 
     expect(() => readProviderConfigs(config)).toThrow(
       `username must be provided when password is defined.`,
