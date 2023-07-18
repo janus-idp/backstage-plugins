9cc05b1647029d66c2dd53a92dc1ed4f46a43ec9
Alberto Gutierrez <aljesusg@gmail.com>
2023-07-18 09:32:52 +0200

    Fix tests

--- plugins/kiali-backend/src/filters/byAnnotation.test.ts
+++ plugins/kiali-backend/src/filters/byAnnotation.test.ts
@@ -14,10 +14,17 @@ const namespaceMock: Namespace[] = [
   {
     name: 'travel-agency',
     cluster: 'kubernetes',
+    labels: {
+      'backstage.io/kubernetes-id': 'travels',
+      app: 'travels',
+    },
   },
   {
     name: 'travel-control',
     cluster: 'kubernetes',
+    labels: {
+      app: 'travels',
+    },
   },
 ];
 
@@ -30,6 +37,26 @@ describe('filterNsByAnnotation', () => {
       );
       expect(result.length).toEqual(1);
       expect(result[0].name).toEqual('bookinfo');
+    });
+  });
+
+  describe('filterById', () => {
+    it('returns travel-agency', async () => {
+      const result = exportedForTesting.filterById(namespaceMock, 'travels');
+      expect(result.length).toEqual(1);
+      expect(result[0].name).toEqual('travel-agency');
+    });
+  });
+
+  describe('filterBySelector', () => {
+    it('returns travel-agency and travel-control', async () => {
+      const result = exportedForTesting.filterBySelector(
+        namespaceMock,
+        'app=travels',
+      );
+      expect(result.length).toEqual(2);
+      expect(result[0].name).toEqual('travel-agency');
+      expect(result[1].name).toEqual('travel-control');
     });
   });
 });
