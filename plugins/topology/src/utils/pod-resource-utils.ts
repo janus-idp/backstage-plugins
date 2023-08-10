c4c428c44d9fec1fdfb5fe1dd5205c3abc6b5cd1
Debsmita Santra <debsmita.santra@gmail.com>
2023-08-10 21:17:31 +0530

    fix(topology): fix pod status info in topology side panel (#620)

--- plugins/topology/src/utils/pod-resource-utils.ts
+++ plugins/topology/src/utils/pod-resource-utils.ts
@@ -1,4 +1,6 @@
 import {
+  V1ContainerState,
+  V1ContainerStatus,
   V1CronJob,
   V1DaemonSet,
   V1Deployment,
@@ -7,7 +9,7 @@ import {
   V1StatefulSet,
 } from '@kubernetes/client-node';
 
-import { AllPodStatus } from '../components/Pods/pod';
+import { AllPodStatus, PodPhase } from '../components/Pods/pod';
 import { ReplicaSetGVK, StatefulSetGVK } from '../models';
 import { PodControllerOverviewItem, PodRCData } from '../types/pods';
 import {
@@ -375,4 +377,81 @@ export const getPodsDataForResource = (
         pods: getPodsForResource(resource, resources),
       };
   }
+};
+
+// This logic is replicated from k8s (at this writing, Kubernetes 1.17)
+// (See https://github.com/kubernetes/kubernetes/blob/release-1.17/pkg/printers/internalversion/printers.go)
+export const podPhase = (pod: V1Pod): PodPhase => {
+  if (!pod) {
+    return '';
+  }
+
+  if (pod?.metadata?.deletionTimestamp) {
+    return 'Terminating';
+  }
+
+  if (!pod.status) {
+    return '';
+  }
+
+  if (pod.status.reason === 'NodeLost') {
+    return 'Unknown';
+  }
+
+  if (pod.status.reason === 'Evicted') {
+    return 'Evicted';
+  }
+
+  let initializing = false;
+  let phase = pod.status.phase || pod.status.reason || null;
+
+  pod.status.initContainerStatuses?.forEach(
+    (container: V1ContainerStatus, i: number) => {
+      const { terminated, waiting } = container.state as V1ContainerState;
+      if (terminated && terminated.exitCode === 0) {
+        return true;
+      }
+
+      initializing = true;
+      if (terminated?.reason) {
+        phase = `Init:${terminated.reason}`;
+      } else if (terminated && !terminated.reason) {
+        phase = terminated.signal
+          ? `Init:Signal:${terminated.signal}`
+          : `Init:ExitCode:${terminated.exitCode}`;
+      } else if (waiting?.reason && waiting.reason !== 'PodInitializing') {
+        phase = `Init:${waiting.reason}`;
+      } else {
+        phase = `Init:${i}/${pod?.status?.initContainerStatuses?.length}`;
+      }
+      return false;
+    },
+  );
+
+  if (!initializing) {
+    let hasRunning = false;
+    const containerStatuses = pod.status.containerStatuses || [];
+    for (let i = containerStatuses.length - 1; i >= 0; i--) {
+      const { state, ready } = containerStatuses[i];
+      if (state?.terminated?.reason) {
+        phase = state?.terminated.reason;
+      } else if (state?.waiting?.reason) {
+        phase = state?.waiting.reason;
+      } else if (state?.waiting && !state.waiting?.reason) {
+        phase = state?.terminated?.signal
+          ? `Signal:${state?.terminated.signal}`
+          : `ExitCode:${state?.terminated?.exitCode}`;
+      } else if (state?.running && ready) {
+        hasRunning = true;
+      }
+    }
+
+    // Change pod status back to "Running" if there is at least one container
+    // still reporting as "Running" status.
+    if (phase === 'Completed' && hasRunning) {
+      phase = 'Running';
+    }
+  }
+
+  return phase;
 };
