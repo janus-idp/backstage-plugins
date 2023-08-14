c2b0d159f07e952e2bc2f241443a66e077d4e8ff
Frank Kong <frkong@redhat.com>
2023-08-14 17:53:30 -0400

    chore(segment): remove deprecated plugin

--- plugins/analytics-provider-segment/src/index.ts
+++ plugins/analytics-provider-segment/src/index.ts
@@ -13,5 +13,4 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-export { analyticsModuleSegment } from './plugin';
 export * from './apis/implementations/AnalyticsApi';
