dd8dd0dacfce0f34d80e450737b9ae6a7c23ec6b
Paul Schultz <pschultz@pobox.com>
2023-08-16 09:39:51 -0500

    remove shared-react changes
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- plugins/shared-react/src/utils/date-utils/format-date.ts
+++
@@ -1,9 +0,0 @@
-import { format } from 'date-fns';
-
-export function formatDate(date: string | undefined) {
-  if (!date) {
-    return 'N/A';
-  }
-
-  return format(new Date(date), 'LLL d, yyyy, h:mm a');
-}
