decf2d95df4184bcf123c43634a787dd797e80d9
Francisco Meneses <fmeneses@redhat.com>
2023-08-17 17:38:06 -0400

    fix(keycloak): fix config loader issue on backstage v1.17

--- plugins/keycloak-backend/src/lib/types.ts
+++ plugins/keycloak-backend/src/lib/types.ts
@@ -68,3 +68,54 @@ export type GroupTransformer = (
   group: GroupRepresentation,
   realm: string,
 ) => Promise<GroupEntity | undefined>;
+
+export type KeycloakOrgConfig = {
+  /**
+   * Location of the Keycloak instance
+   */
+  baseUrl: string;
+  /**
+   * Keycloak realm name. This realm is scraped and entities are
+   */
+  realm?: string;
+  /**
+   * Keycloak realm name. This realm is used for authentication using the credentials below.
+   */
+  loginRealm?: string;
+  /**
+   * Keycloak credentials. Use together with "password".
+   */
+  username: string;
+  /**
+   * Keycloak credentials. Use together with "username".
+   * @visibility secret
+   */
+  password: string;
+  /**
+   * Keycloak credentials. Use together with "clientSecret".
+   */
+  clientId: string;
+  /**
+   * Keycloak credentials. Use together with "clientId".
+   * @visibility secret
+   */
+  clientSecret: string;
+
+  /**
+   * The number of users to query at a time.
+   * @defaultValue 100
+   * @remarks
+   * This is a performance optimization to avoid querying too many users at once.
+   * @see https://www.keycloak.org/docs-api/11.0/rest-api/index.html#_users_resource
+   */
+  userQuerySize?: number;
+
+  /**
+   * The number of groups to query at a time.
+   * @defaultValue 100
+   * @remarks
+   * This is a performance optimization to avoid querying too many groups at once.
+   * @see https://www.keycloak.org/docs-api/11.0/rest-api/index.html#_groups_resource
+   */
+  groupQuerySize?: number;
+};
