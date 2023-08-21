add46e92718a8dc435d3eeae04a282fd26a90ffa
Navinya Shende <nshende@redhat.com>
2023-08-21 17:38:33 +0530

    removed dead code

--- plugins/shared-react/src/utils/date/format-date.ts
+++ plugins/shared-react/src/utils/date/format-date.ts
@@ -1,16 +1,3 @@
-/* export function formatDate(date: string | number | Date) {
-  if (date === -1) {
-    return 'N/A';
-  }
-
-  const adjustedDate = typeof date === 'number' ? date * 1000 : date;
-  return new Date(adjustedDate).toLocaleString('en-US', {
-    timeZone: new Intl.DateTimeFormat().resolvedOptions().timeZone,
-    timeStyle: 'short',
-    dateStyle: 'medium',
-  });
-} */
-
 import { format } from 'date-fns';
 
 /**
--- plugins/shared-react/src/utils/user-timezone/user-timezone.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Gets the user time zone
- *
- * @return The users time zone as a string or 'UTC' by default
- */
-export function getUserTimeZone(): string {
-  return new Intl.DateTimeFormat().resolvedOptions().timeZone || 'UTC';
-}
