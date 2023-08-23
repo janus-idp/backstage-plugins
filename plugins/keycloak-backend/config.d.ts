587b40aa7bd824ae266e0c3b89025195deaff119
Paul Schultz <pschultz@pobox.com>
2023-08-23 09:34:07 -0500

    fix(config types): inline types in config.d.ts
    Signed-off-by: Paul Schultz <pschultz@pobox.com>

--- plugins/keycloak-backend/config.d.ts
+++ plugins/keycloak-backend/config.d.ts
@@ -1,15 +1,71 @@
 import { TaskScheduleDefinitionConfig } from '@backstage/backend-tasks';
 
-import { KeycloakOrgConfig } from './src/lib/types';
-
 export interface Config {
   catalog?: {
     providers?: {
       keycloakOrg?: {
-        [key: string]: (
-          | Omit<KeycloakOrgConfig, 'username' | 'password'>
-          | Omit<KeycloakOrgConfig, 'clientId' | 'clientSecret'>
-        ) & { scheduler: TaskScheduleDefinitionConfig };
+        [key: string]: {
+          /**
+           * KeycloakOrgConfig
+           */
+          /**
+           * Location of the Keycloak instance
+           */
+          baseUrl: string;
+          /**
+           * Keycloak realm name. This realm is scraped and entities are
+           */
+          realm?: string;
+          /**
+           * Keycloak realm name. This realm is used for authentication using the credentials below.
+           */
+          loginRealm?: string;
+          /**
+           * The number of users to query at a time.
+           * @defaultValue 100
+           * @remarks
+           * This is a performance optimization to avoid querying too many users at once.
+           * @see https://www.keycloak.org/docs-api/11.0/rest-api/index.html#_users_resource
+           */
+          userQuerySize?: number;
+          /**
+           * The number of groups to query at a time.
+           * @defaultValue 100
+           * @remarks
+           * This is a performance optimization to avoid querying too many groups at once.
+           * @see https://www.keycloak.org/docs-api/11.0/rest-api/index.html#_groups_resource
+           */
+          groupQuerySize?: number;
+        } & { scheduler: TaskScheduleDefinitionConfig } & (
+            | {
+                /**
+                 * KeycloakClientCredentials
+                 */
+                /**
+                 * Keycloak credentials. Use together with "password".
+                 */
+                username: string;
+                /**
+                 * Keycloak credentials. Use together with "username".
+                 * @visibility secret
+                 */
+                password: string;
+              }
+            | {
+                /**
+                 * KeycloakClientCredentials
+                 */
+                /**
+                 * Keycloak credentials. Use together with "clientSecret".
+                 */
+                clientId: string;
+                /**
+                 * Keycloak credentials. Use together with "clientId".
+                 * @visibility secret
+                 */
+                clientSecret: string;
+              }
+          );
       };
     };
   };
