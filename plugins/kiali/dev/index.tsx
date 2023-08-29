93edf9b51cff2207646900bead5a57dbac80c4ec
Alberto Gutierrez <aljesusg@gmail.com>
2023-08-29 14:42:02 +0200

    Add namespaces to dev

--- plugins/kiali/dev/index.tsx
+++ plugins/kiali/dev/index.tsx
@@ -15,6 +15,7 @@ import { KialiPage, kialiPlugin } from '../src/plugin';
 import overviewJson from './__fixtures__/1-overview.json';
 import configJson from './__fixtures__/config.json';
 import statusJson from './__fixtures__/status.json';
+import namespacesJson from './__fixtures__/namespaces.json';
 
 const mockEntity: Entity = {
   apiVersion: 'backstage.io/v1alpha1',
@@ -37,15 +38,18 @@ class MockKialiClient implements KialiApi {
   readonly resource: FetchResponse;
   readonly status: FetchResponse;
   readonly config: FetchResponse;
+  readonly namespaces: FetchResponse;
 
   constructor(
     fixtureData: any,
     status: any = statusJson,
     config: any = configJson,
+    namespaces: any = namespacesJson
   ) {
     this.resource = fixtureData;
     this.status = status;
     this.config = config;
+    this.namespaces = namespaces
   }
 
   setEntity(_: Entity): void {}
@@ -55,6 +59,14 @@ class MockKialiClient implements KialiApi {
       errors: [],
       warnings: [],
       response: this.config,
+    };
+  }
+
+  async getNamespaces(): Promise<FetchResponseWrapper> {
+    return {
+      errors: [],
+      warnings: [],
+      response: this.namespaces,
     };
   }
 
