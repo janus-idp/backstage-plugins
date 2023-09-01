8adcf17d6e0380aa52148f0cc09f22dea9a1c49a
Frank Kong <frkong@redhat.com>
2023-09-01 11:26:45 -0400

    chore(jfrog): update dev environment to not crash on load

--- plugins/jfrog-artifactory/dev/index.tsx
+++ plugins/jfrog-artifactory/dev/index.tsx
@@ -1,13 +1,36 @@
 import React from 'react';
 
+import { Entity } from '@backstage/catalog-model';
 import { createDevApp } from '@backstage/dev-utils';
+import { EntityProvider } from '@backstage/plugin-catalog-react';
 
 import { JfrogArtifactoryPage, jfrogArtifactoryPlugin } from '../src/plugin';
+
+const mockEntity: Entity = {
+  apiVersion: 'backstage.io/v1alpha1',
+  kind: 'Component',
+  metadata: {
+    name: 'backstage',
+    description: 'backstage.io',
+    annotations: {
+      'jfrog-artifactory/image-name': 'backstage',
+    },
+  },
+  spec: {
+    lifecycle: 'production',
+    type: 'service',
+    owner: 'user:guest',
+  },
+};
 
 createDevApp()
   .registerPlugin(jfrogArtifactoryPlugin)
   .addPage({
-    element: <JfrogArtifactoryPage />,
+    element: (
+      <EntityProvider entity={mockEntity}>
+        <JfrogArtifactoryPage />
+      </EntityProvider>
+    ),
     title: 'Root Page',
     path: '/artifactory',
   })
