8384b67701ac8c09755772ae198e304b1cf13e79
debsmita1 <debsmita.santra@gmail.com>
2023-09-14 19:11:50 +0530

    fix(tekton): fix pipelinerun status icon color

--- plugins/tekton/src/components/common/index.ts
+++ plugins/tekton/src/components/common/index.ts
@@ -2,4 +2,3 @@ export { ErrorPanel } from './ErrorPanel';
 export { ClusterSelector } from './ClusterSelector';
 export { Status } from './Status';
 export { ResourceStatus } from './ResourceStatus';
-export { StatusIcon } from './StatusIcon';
