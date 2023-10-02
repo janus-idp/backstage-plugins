92b922ca9239a0d12972a850cc0a2a54d5b71ea6
Marek Libra <marek.libra@gmail.com>
2023-10-02 11:23:00 +0200

    Read notifications from backend

--- plugins/parodos-notifications/src/components/ParodosNotificationsTable/ParodosNotificationsTable.tsx
+++ plugins/parodos-notifications/src/components/ParodosNotificationsTable/ParodosNotificationsTable.tsx
@@ -7,23 +7,12 @@ import {
   Table,
   TableColumn,
 } from '@backstage/core-components';
+import { configApiRef, useApi } from '@backstage/core-plugin-api';
+
+import { listNotifications } from '../../notificationsService';
+import { Notification } from '../../types';
 
 // import { makeStyles } from '@material-ui/core/styles';
-
-export const exampleData = {
-  results: [
-    {
-      id: 1,
-      subject: 'my first message',
-      body: 'I have nothing to talk about',
-    },
-    {
-      id: 2,
-      subject: 'my second message',
-      body: 'I still dont have anything to talk about',
-    },
-  ],
-};
 
 // const useStyles = makeStyles({
 //   avatar: {
@@ -32,12 +21,6 @@ export const exampleData = {
 //     borderRadius: '50%',
 //   },
 // });
-
-type Notification = {
-  id: string | number;
-  subject: string;
-  body: string;
-};
 
 type DenseTableProps = {
   notifications: Notification[];
@@ -69,12 +52,13 @@ export const DenseTable = ({ notifications }: DenseTableProps) => {
 };
 
 export const ParodosNotificationsTable = () => {
-  const { value, loading, error } = useAsync(async (): Promise<
-    Notification[]
-  > => {
-    // Would use fetch in a real world example
-    return exampleData.results;
-  }, []);
+  const config = useApi(configApiRef);
+  const backendUrl = config.getString('backend.baseUrl');
+
+  const { value, loading, error } = useAsync(
+    async (): Promise<Notification[]> => listNotifications(backendUrl),
+    [backendUrl],
+  );
 
   if (loading) {
     return <Progress />;
