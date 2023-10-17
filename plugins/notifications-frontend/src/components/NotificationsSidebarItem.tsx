c4bf49be9ecbf653a1bc7797d0011acecc4c6fb0
Marek Libra <marek.libra@gmail.com>
2023-10-17 13:34:43 +0200

    Mark "unread" notifications in the left-side menu

--- plugins/notifications-frontend/src/components/NotificationsSidebarItem.tsx
+++ plugins/notifications-frontend/src/components/NotificationsSidebarItem.tsx
@@ -1,15 +1,50 @@
 import React from 'react';
 
 import { SidebarItem } from '@backstage/core-components';
+import { useApi } from '@backstage/core-plugin-api';
 
+import { Tooltip } from '@material-ui/core';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import NotificationsOffIcon from '@material-ui/icons/NotificationsOff';
 
+import { notificationsApiRef } from '../api';
 import { NOTIFICATIONS_ROUTE } from '../constants';
+import { usePollingEffect } from './usePollingEffect';
 
-export const NotificationsSidebarItem = () => (
-  <SidebarItem
-    icon={NotificationsIcon}
-    to={NOTIFICATIONS_ROUTE}
-    text="Notifications"
-  />
+const NotificationsErrorIcon = () => (
+  <Tooltip title="Failed to load notifications">
+    <NotificationsOffIcon />
+  </Tooltip>
 );
+
+export const NotificationsSidebarItem = () => {
+  const notificationsApi = useApi(notificationsApiRef);
+  const [error, setError] = React.useState<Error | undefined>(undefined);
+  const [unreadCount, setUnreadCount] = React.useState(0);
+
+  const pollCallback = React.useCallback(async () => {
+    try {
+      setUnreadCount(await notificationsApi.getUnreadCount(/* params */));
+    } catch (e: unknown) {
+      setError(e as Error);
+    }
+  }, [notificationsApi]);
+
+  usePollingEffect(pollCallback, [
+    /* params */
+  ]);
+
+  let icon = NotificationsIcon;
+  if (!!error) {
+    icon = NotificationsErrorIcon;
+  }
+
+  return (
+    <SidebarItem
+      icon={icon}
+      to={NOTIFICATIONS_ROUTE}
+      text="Notifications"
+      hasNotifications={!error && !!unreadCount}
+    />
+  );
+};
