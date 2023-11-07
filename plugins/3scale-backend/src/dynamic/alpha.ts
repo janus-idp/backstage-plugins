176f965b2565bbf81c130d30b21096ef2e70c9ab
David Festal <dfestal@redhat.com>
2023-11-07 12:33:06 +0100

    feat(dynamic-plugins): make 3scale backend plugin dynamic
    Signed-off-by: David Festal <dfestal@redhat.com>

---
+++ plugins/3scale-backend/src/dynamic/alpha.ts
@@ -0,0 +1,36 @@
+import { loggerToWinstonLogger } from '@backstage/backend-common';
+import {
+  coreServices,
+  createBackendModule,
+} from '@backstage/backend-plugin-api';
+import { BackendDynamicPluginInstaller } from '@backstage/backend-plugin-manager';
+import { catalogProcessingExtensionPoint } from '@backstage/plugin-catalog-node/alpha';
+
+import { ThreeScaleApiEntityProvider } from '../providers';
+
+export const dynamicPluginInstaller: BackendDynamicPluginInstaller = {
+  kind: 'new',
+
+  install: createBackendModule({
+    moduleId: 'catalog-backend-module-3scale',
+    pluginId: 'catalog',
+    register(env) {
+      env.registerInit({
+        deps: {
+          catalog: catalogProcessingExtensionPoint,
+          config: coreServices.rootConfig,
+          logger: coreServices.logger,
+          scheduler: coreServices.scheduler,
+        },
+        async init({ catalog, config, logger, scheduler }) {
+          catalog.addEntityProvider(
+            ThreeScaleApiEntityProvider.fromConfig(config, {
+              logger: loggerToWinstonLogger(logger),
+              scheduler: scheduler,
+            }),
+          );
+        },
+      });
+    },
+  }),
+};
