3d96032920f442df38213637362c60c50a536246
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2023-11-28 07:19:28 -0300

    fix(orchestrator): make the port config optional (#37)

--- plugins/orchestrator-backend/src/service/SonataFlowService.ts
+++ plugins/orchestrator-backend/src/service/SonataFlowService.ts
@@ -34,7 +34,7 @@ interface LauncherCommand {
 
 interface SonataFlowConnectionConfig {
   host: string;
-  port: number;
+  port?: number;
   containerImage: string;
   resourcesPath: string;
   persistencePath: string;
@@ -62,6 +62,9 @@ export class SonataFlowService {
   }
 
   public get url(): string {
+    if (!this.connection.port) {
+      return this.connection.host;
+    }
     return `${this.connection.host}:${this.connection.port}`;
   }
 
@@ -335,7 +338,7 @@ export class SonataFlowService {
     }
 
     launcherArgs.push('--rm');
-    launcherArgs.push('-p', `${this.connection.port}:8080`);
+    launcherArgs.push('-p', `${this.connection.port ?? 80}:8080`);
     launcherArgs.push(
       '-v',
       `${resourcesAbsPath}:${SONATA_FLOW_RESOURCES_PATH}`,
@@ -368,7 +371,9 @@ export class SonataFlowService {
       false;
 
     const host = config.getString('orchestrator.sonataFlowService.baseUrl');
-    const port = config.getNumber('orchestrator.sonataFlowService.port');
+    const port = config.getOptionalNumber(
+      'orchestrator.sonataFlowService.port',
+    );
 
     const resourcesPath =
       config.getOptionalString(
