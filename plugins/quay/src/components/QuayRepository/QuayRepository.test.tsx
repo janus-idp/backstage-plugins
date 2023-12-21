2c050dbcfe4fae55ce89f9a8b54c8c4550b804f8
Rohit Rai <rohitkrai03@gmail.com>
2023-12-21 17:36:17 +0530

    fix(quay): fix infinite progress bar when scan is unsupported (#1031)

--- plugins/quay/src/components/QuayRepository/QuayRepository.test.tsx
+++ plugins/quay/src/components/QuayRepository/QuayRepository.test.tsx
@@ -96,4 +96,28 @@ describe('QuayRepository', () => {
     expect(queryByText(/Quay repository/i)).toBeInTheDocument();
     expect(queryByTestId('quay-repo-security-scan-progress')).not.toBeNull();
   });
+
+  it('should show table if loaded and data is present but shows unsupported if security scan is not supported', () => {
+    (useTags as jest.Mock).mockReturnValue({
+      loading: false,
+      data: [
+        {
+          name: 'latest',
+          manifest_digest: undefined,
+          securityStatus: 'unsupported',
+          size: null,
+          last_modified: 'Wed, 15 Mar 2023 18:22:18 -0000',
+        },
+      ],
+    });
+    const { queryByTestId, queryByText } = render(
+      <BrowserRouter>
+        <QuayRepository />
+      </BrowserRouter>,
+    );
+    expect(queryByTestId('quay-repo-table')).not.toBeNull();
+    expect(queryByTestId('quay-repo-table-empty')).toBeNull();
+    expect(queryByText(/Quay repository/i)).toBeInTheDocument();
+    expect(queryByTestId('quay-repo-security-scan-unsupported')).not.toBeNull();
+  });
 });
