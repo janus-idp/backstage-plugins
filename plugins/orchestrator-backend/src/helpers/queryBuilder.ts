6de6ea37ca8269d237a548ce4aca4723823e0e4c
jude <jude.niroshan11@gmail.com>
2024-02-02 15:28:07 +0530

    FLPATH-749 support pagination

---
+++ plugins/orchestrator-backend/src/helpers/queryBuilder.ts
@@ -0,0 +1,37 @@
+import { Pagination } from '../types/pagination';
+
+export function buildGraphQlQuery(
+  type: string,
+  queryBody: string,
+  whereClause?: string,
+  pagination?: Pagination,
+): string {
+  let query = `{${type}`;
+
+  if (whereClause || pagination) {
+    query += ` (`;
+
+    if (whereClause) {
+      query += `where: {${whereClause}}`;
+      if (pagination) {
+        query += `, `;
+      }
+    }
+    if (pagination) {
+      if (pagination.sortField) {
+        query += `orderBy: {${
+          pagination.sortField
+        }: ${pagination.order?.toUpperCase()}}, `;
+      }
+      query += `pagination: {limit: ${pagination.limit} , offset: ${pagination.offset}}`;
+    }
+
+    query += `) `;
+  }
+  query += ` {`;
+  query += queryBody;
+  query += ` }`;
+  query += ` }`;
+
+  return query;
+}
--- plugins/orchestrator-backend/src/service/router.ts
+++ plugins/orchestrator-backend/src/service/router.ts
@@ -21,6 +21,7 @@ import {
 
 import { RouterArgs } from '../routerWrapper';
 import { ApiResponseBuilder } from '../types/apiResponse';
+import { buildPagination } from '../types/pagination';
 import { V1 } from './api/v1';
 import { V2 } from './api/v2';
 import { CloudEventService } from './CloudEventService';
@@ -341,8 +342,8 @@ function setupInternalRoutes(
     },
   );
 
-  router.get('/instances', async (_, res) => {
-    await V1.getInstances(services.dataIndexService)
+  router.get('/instances', async (req, res) => {
+    await V1.getInstances(services.dataIndexService, buildPagination(req))
       .then(result => res.status(200).json(result))
       .catch(error => {
         res.status(500).send(error.message || 'internal Server Error');
@@ -353,7 +354,7 @@ function setupInternalRoutes(
   api.register(
     'getInstances',
     async (_c, _req: express.Request, res: express.Response, next) => {
-      await V2.getInstances(services.dataIndexService)
+      await V2.getInstances(services.dataIndexService, buildPagination(req))
         .then(result => res.json(result))
         .catch(next);
     },
@@ -407,8 +408,10 @@ function setupInternalRoutes(
       params: { instanceId },
     } = req;
 
-    const jobs =
-      await services.dataIndexService.fetchProcessInstanceJobs(instanceId);
+    const jobs = await services.dataIndexService.fetchProcessInstanceJobs(
+      instanceId,
+      buildPagination(req),
+    );
 
     if (!jobs) {
       res.status(500).send(`Couldn't fetch jobs for instance ${instanceId}`);
