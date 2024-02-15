3494257c4e394114ec125fa2e77148b1ed5f01b2
jude <jude.niroshan11@gmail.com>
2024-02-15 16:43:05 +0100

    defined constants

--- plugins/orchestrator-backend/src/service/constants.ts
+++ plugins/orchestrator-backend/src/service/constants.ts
@@ -1,1 +1,5 @@
 export const WORKFLOW_DATA_KEY = 'workflowdata';
+export const DEFAULT_PAGE_NUMBER = 0;
+export const DEFAULT_PAGE_SIZE = 10;
+export const DEFAULT_SORT_FIELD = undefined;
+export const DEFAULT_SORT_ORDER = 'ASC';
