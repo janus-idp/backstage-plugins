cd1bebc23eac4f602da0bc39c8fc5f69f67f1343
debsmita1 <debsmita.santra@gmail.com>
2024-02-26 16:33:56 +0530

    feat(bulk-import): create bulk-import frontend plugin

---
+++ plugins/bulk-import/src/components/Repositories/RepositoriesList.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import { Table } from '@backstage/core-components';
+
+import { makeStyles } from '@material-ui/core';
+
+import { RepositoriesData } from '../../types';
+import { columns } from './RepositoriesListColumns';
+
+const useStyles = makeStyles(theme => ({
+  empty: {
+    padding: theme.spacing(2),
+    display: 'flex',
+    justifyContent: 'center',
+  },
+}));
+
+export const RepositoriesList = () => {
+  const classes = useStyles();
+  const data: RepositoriesData[] = [];
+
+  return (
+    <Table
+      title="Added repositories (0)"
+      options={{ padding: 'default', search: true, paging: true }}
+      data={data}
+      isLoading={false}
+      columns={columns}
+      emptyContent={
+        <div data-testid="repositories-table-empty" className={classes.empty}>
+          No records found
+        </div>
+      }
+    />
+  );
+};
