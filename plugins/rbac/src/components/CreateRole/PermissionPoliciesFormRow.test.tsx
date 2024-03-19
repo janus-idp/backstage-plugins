55929d5d862cc56f85e9dc490fb597480e31d4be
Yi Cai <yicai@redhat.com>
2024-03-19 07:37:24 -0400

    chore(rbac): increase rbac test coverage (#1345)
    * chore(rbac): increase rbac test coverage
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * chore(rbac): increase rbac test coverage
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * Fixed PR task erros
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    * Minor fix
    
    Signed-off-by: Yi Cai <yicai@redhat.com>
    
    ---------
    
    Signed-off-by: Yi Cai <yicai@redhat.com>

---
+++ plugins/rbac/src/components/CreateRole/PermissionPoliciesFormRow.test.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+import { fireEvent, render, screen, within } from '@testing-library/react';
+
+import '@testing-library/jest-dom';
+
+import { PermissionPoliciesFormRow } from './PermissionPoliciesFormRow';
+
+describe('PermissionPoliciesFormRow', () => {
+  const mockProps = {
+    permissionPoliciesRowData: { plugin: '', permission: '', policies: [] },
+    permissionPoliciesData: {
+      plugins: ['', 'Plugin1', 'Plugin2'],
+      pluginsPermissions: {
+        Plugin1: { permissions: ['Permission1', 'Permission2'], policies: {} },
+        Plugin2: { permissions: ['Permission1', 'Permission2'], policies: {} },
+      },
+    },
+    permissionPoliciesRowError: { plugin: '', permission: '' },
+    rowCount: 2,
+    rowName: 'testRow',
+    onRemove: jest.fn(),
+    onChangePlugin: jest.fn(),
+    onChangePermission: jest.fn(),
+    onChangePolicy: jest.fn(),
+    handleBlur: jest.fn(),
+    getPermissionDisabled: jest.fn().mockReturnValue(false),
+  };
+
+  it('renders without crashing', () => {
+    render(<PermissionPoliciesFormRow {...mockProps} />);
+    expect(screen.queryByText(/Plugin/)).toBeInTheDocument();
+    expect(screen.queryByText(/Permission/)).toBeInTheDocument();
+  });
+
+  it('calls onRemove when remove button is clicked', () => {
+    const { getByTitle } = render(<PermissionPoliciesFormRow {...mockProps} />);
+    const removeButton = getByTitle('Remove');
+    fireEvent.click(removeButton);
+    expect(mockProps.onRemove).toHaveBeenCalled();
+  });
+
+  it('calls onChangePlugin when a plugin is selected', async () => {
+    const { getByLabelText, findByText } = render(
+      <PermissionPoliciesFormRow {...mockProps} />,
+    );
+
+    fireEvent.mouseDown(getByLabelText(/Plugin/));
+    const pluginOption = await findByText('Plugin1');
+    const listboxElement = pluginOption.closest('ul');
+
+    if (!listboxElement) {
+      throw new Error('Unable to find the listbox element.');
+    }
+
+    const listbox = within(listboxElement);
+    fireEvent.click(listbox.getByText(/Plugin1/));
+
+    expect(mockProps.onChangePlugin).toHaveBeenCalledWith('Plugin1');
+  });
+});
