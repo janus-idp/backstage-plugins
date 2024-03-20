6ad7053ce68bec75d3c463a3b591067b129e54fb
Gloria Ciavarrini <gciavarrini@redhat.com>
2024-03-20 12:30:14 +0100

    chore(orchestrator): set npx packages version for openapi generation (#1353)
    * chore(orchestrator): don't prompt the user for the installation
    
    Add `--yes` flag to npx commands so that the script
    doesn't need to prompt the user for the installation
    
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>
    
    * chore(orchestrator): fix version packages
    
    If we don't specify the version, npx will use the latest one,
    which might lead to not deterministic results.
    
    yaml2json package is 10 years old can't fix a version.
    
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>
    
    ---------
    
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- plugins/orchestrator-common/scripts/openapi.sh
+++ plugins/orchestrator-common/scripts/openapi.sh
@@ -8,9 +8,9 @@ DEFINITION_FILE="./src/auto-generated/api/definition.ts"
 METADATA_FILE="./src/auto-generated/.METADATA.sha1"
 
 openapi_generate() {
-    npx openapi-typescript ${OPENAPI_SPEC_FILE} -o ${SCHEMA_FILE}
-    npx openapi-generator-cli generate -g asciidoc -i ./src/openapi/openapi.yaml -o ./src/auto-generated/docs/index.adoc
-    npx yaml2json -f ${OPENAPI_SPEC_FILE}
+    npx --yes openapi-typescript@6.7.5 ${OPENAPI_SPEC_FILE} -o ${SCHEMA_FILE}
+    npx --yes @openapitools/openapi-generator-cli@v2.13.1 generate -g asciidoc -i ./src/openapi/openapi.yaml -o ./src/auto-generated/docs/index.adoc
+    npx --yes --package=js-yaml-cli@0.6.0 -- yaml2json -f ${OPENAPI_SPEC_FILE}
 
     OPENAPI_SPEC_FILE_JSON=$(tr -d '[:space:]' < "$(dirname $OPENAPI_SPEC_FILE)"/openapi.json)
     cat << EOF > ${DEFINITION_FILE}
