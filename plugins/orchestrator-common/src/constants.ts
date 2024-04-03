d59ad044cd5d8d7566464f140cdbc1dfbad85a62
Guilherme Caponetto <638737+caponetto@users.noreply.github.com>
2024-04-03 09:20:50 -0300

    fix(orchestrator): update devmode container tag (#1439)

--- plugins/orchestrator-common/src/constants.ts
+++ plugins/orchestrator-common/src/constants.ts
@@ -1,6 +1,6 @@
 // Default values for the orchestrator plugin configuration
 export const DEFAULT_SONATAFLOW_CONTAINER_IMAGE =
-  'quay.io/kiegroup/kogito-swf-devmode-nightly:main-2024-02-19';
+  'quay.io/kiegroup/kogito-swf-devmode-nightly:main-2024-04-03';
 export const DEFAULT_SONATAFLOW_PERSISTANCE_PATH = '/home/kogito/persistence';
 export const DEFAULT_EDITOR_PATH =
   'https://sandbox.kie.org/swf-chrome-extension/0.32.0';
