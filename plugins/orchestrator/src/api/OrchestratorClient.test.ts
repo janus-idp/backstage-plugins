729baf57119a3c998e5f93634fd3939c10afc26d
Gloria Ciavarrini <gciavarrini@redhat.com>
2024-05-13 14:06:26 +0200

    Add unit tests for abortWorkflow
    Signed-off-by: Gloria Ciavarrini <gciavarrini@redhat.com>

--- plugins/orchestrator/src/api/OrchestratorClient.test.ts
+++ plugins/orchestrator/src/api/OrchestratorClient.test.ts
@@ -1,4 +1,5 @@
 import { DiscoveryApi } from '@backstage/core-plugin-api';
+import { ResponseError } from '@backstage/errors';
 import { JsonObject } from '@backstage/types';
 
 import { WorkflowExecutionResponse } from '@janus-idp/backstage-plugin-orchestrator-common';
@@ -11,6 +12,8 @@ import {
 describe('OrchestratorClient', () => {
   let mockDiscoveryApi: jest.Mocked<DiscoveryApi>;
   let orchestratorClientOptions: jest.Mocked<OrchestratorClientOptions>;
+  let orchestratorClient: OrchestratorClient;
+  const baseUrl = 'https://api.example.com';
 
   const mockFetch = jest.fn();
   (global as any).fetch = mockFetch; // Cast global to any to avoid TypeScript errors
@@ -26,127 +29,167 @@ describe('OrchestratorClient', () => {
     orchestratorClientOptions = {
       discoveryApi: mockDiscoveryApi,
     };
+    orchestratorClient = new OrchestratorClient(orchestratorClientOptions);
   });
-  it('should execute workflow with empty parameters', async () => {
-    // Given
-    const baseUrl = 'https://api.example.com';
-    const workflowId = 'workflow123';
-    const executionId = 'execId001';
-    const parameters: JsonObject = {};
-    const args = {
-      workflowId: workflowId,
-      parameters: {} as JsonObject,
-    };
+  describe('executeWorkflow', () => {
+    it('should execute workflow with empty parameters', async () => {
+      // Given
+      const workflowId = 'workflow123';
+      const executionId = 'execId001';
+      const parameters: JsonObject = {};
+      const args = {
+        workflowId: workflowId,
+        parameters: {} as JsonObject,
+      };
 
-    // Mock fetch to simulate a successful response
-    (global.fetch as jest.Mock).mockResolvedValue({
-      ok: true,
-      json: jest.fn().mockResolvedValue({
-        id: executionId,
-      }),
+      // Mock fetch to simulate a successful response
+      (global.fetch as jest.Mock).mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          id: executionId,
+        }),
+      });
+
+      // When
+      const result: WorkflowExecutionResponse =
+        await orchestratorClient.executeWorkflow(args);
+
+      // Then
+      expect(fetch).toHaveBeenCalledWith(
+        `${baseUrl}/workflows/${workflowId}/execute`,
+        {
+          method: 'POST',
+          body: JSON.stringify(parameters),
+          headers: { 'content-type': 'application/json' },
+        },
+      );
+
+      expect(result).toBeDefined();
+      expect(result.id).toBeDefined();
+      expect(result.id).toEqual(executionId);
+    });
+    it('should execute workflow with business key', async () => {
+      // Given
+      const workflowId = 'workflow123';
+      const businessKey = 'business123';
+      const executionId = 'execId001';
+      const parameters: JsonObject = {};
+      const args = {
+        workflowId: workflowId,
+        parameters: {} as JsonObject,
+        businessKey: businessKey,
+      };
+
+      // Mock fetch to simulate a successful response
+      (global.fetch as jest.Mock).mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          id: executionId,
+        }),
+      });
+
+      // When
+      const client = new OrchestratorClient(orchestratorClientOptions);
+      const result: WorkflowExecutionResponse =
+        await client.executeWorkflow(args);
+
+      // Then
+      expect(fetch).toHaveBeenCalledWith(
+        `${baseUrl}/workflows/${workflowId}/execute?businessKey=${businessKey}`,
+        {
+          method: 'POST',
+          body: JSON.stringify(parameters),
+          headers: { 'content-type': 'application/json' },
+        },
+      );
+
+      expect(result).toBeDefined();
+      expect(result.id).toBeDefined();
+      expect(result.id).toEqual(executionId);
+    });
+    it('should execute workflow with parameters and business key', async () => {
+      // Given
+      const workflowId = 'workflow123';
+      const businessKey = 'business123';
+      const executionId = 'execId001';
+      const parameters: JsonObject = {
+        param1: 'one',
+        param2: 2,
+        param3: true,
+      };
+      const args = {
+        workflowId: workflowId,
+        parameters: parameters,
+        businessKey: businessKey,
+      };
+
+      // Mock fetch to simulate a successful response
+      (global.fetch as jest.Mock).mockResolvedValue({
+        ok: true,
+        json: jest.fn().mockResolvedValue({
+          id: executionId,
+        }),
+      });
+
+      // When
+      const client = new OrchestratorClient(orchestratorClientOptions);
+      const result: WorkflowExecutionResponse =
+        await client.executeWorkflow(args);
+
+      // Then
+      expect(fetch).toHaveBeenCalledWith(
+        `${baseUrl}/workflows/${workflowId}/execute?businessKey=${businessKey}`,
+        {
+          method: 'POST',
+          body: JSON.stringify(parameters),
+          headers: { 'content-type': 'application/json' },
+        },
+      );
+
+      expect(result).toBeDefined();
+      expect(result.id).toBeDefined();
+      expect(result.id).toEqual(executionId);
+    });
+  });
+  describe('abortWorkflow', () => {
+    it('should abort a workflow instance successfully', async () => {
+      // Given
+      const instanceId = 'instance123';
+
+      // Mock fetch to simulate a successful response
+      (global.fetch as jest.Mock).mockResolvedValueOnce({
+        ok: true,
+      });
+
+      // When
+      await orchestratorClient.abortWorkflowInstance(instanceId);
+
+      // Assert
+      expect(fetch).toHaveBeenCalledWith(
+        `${baseUrl}/instances/${instanceId}/abort`,
+        {
+          method: 'DELETE',
+          headers: { 'content-type': 'application/json' },
+        },
+      );
     });
 
-    // When
-    const client = new OrchestratorClient(orchestratorClientOptions);
-    const result: WorkflowExecutionResponse =
-      await client.executeWorkflow(args);
+    it('should throw a ResponseError if aborting the workflow instance fails', async () => {
+      // Given
+      const instanceId = 'instance123';
 
-    // Then
-    expect(fetch).toHaveBeenCalledWith(
-      `${baseUrl}/workflows/${workflowId}/execute`,
-      {
-        method: 'POST',
-        body: JSON.stringify(parameters),
-        headers: { 'content-type': 'application/json' },
-      },
-    );
+      // Mock fetch to simulate a failed response
+      (global.fetch as jest.Mock).mockResolvedValueOnce({
+        ok: false,
+        status: 500,
+        statusText: 'Internal Server Error',
+      });
 
-    expect(result).toBeDefined();
-    expect(result.id).toBeDefined();
-    expect(result.id).toEqual(executionId);
-  });
-  it('should execute workflow with business key', async () => {
-    // Given
-    const baseUrl = 'https://api.example.com';
-    const workflowId = 'workflow123';
-    const businessKey = 'business123';
-    const executionId = 'execId001';
-    const parameters: JsonObject = {};
-    const args = {
-      workflowId: workflowId,
-      parameters: {} as JsonObject,
-      businessKey: businessKey,
-    };
+      // When
+      const promise = orchestratorClient.abortWorkflowInstance(instanceId);
 
-    // Mock fetch to simulate a successful response
-    (global.fetch as jest.Mock).mockResolvedValue({
-      ok: true,
-      json: jest.fn().mockResolvedValue({
-        id: executionId,
-      }),
+      // Then
+      await expect(promise).rejects.toThrow();
     });
-
-    // When
-    const client = new OrchestratorClient(orchestratorClientOptions);
-    const result: WorkflowExecutionResponse =
-      await client.executeWorkflow(args);
-
-    // Then
-    expect(fetch).toHaveBeenCalledWith(
-      `${baseUrl}/workflows/${workflowId}/execute?businessKey=${businessKey}`,
-      {
-        method: 'POST',
-        body: JSON.stringify(parameters),
-        headers: { 'content-type': 'application/json' },
-      },
-    );
-
-    expect(result).toBeDefined();
-    expect(result.id).toBeDefined();
-    expect(result.id).toEqual(executionId);
-  });
-  it('should execute workflow with parameters and business key', async () => {
-    // Given
-    const baseUrl = 'https://api.example.com';
-    const workflowId = 'workflow123';
-    const businessKey = 'business123';
-    const executionId = 'execId001';
-    const parameters: JsonObject = {
-      param1: 'one',
-      param2: 2,
-      param3: true,
-    };
-    const args = {
-      workflowId: workflowId,
-      parameters: parameters,
-      businessKey: businessKey,
-    };
-
-    // Mock fetch to simulate a successful response
-    (global.fetch as jest.Mock).mockResolvedValue({
-      ok: true,
-      json: jest.fn().mockResolvedValue({
-        id: executionId,
-      }),
-    });
-
-    // When
-    const client = new OrchestratorClient(orchestratorClientOptions);
-    const result: WorkflowExecutionResponse =
-      await client.executeWorkflow(args);
-
-    // Then
-    expect(fetch).toHaveBeenCalledWith(
-      `${baseUrl}/workflows/${workflowId}/execute?businessKey=${businessKey}`,
-      {
-        method: 'POST',
-        body: JSON.stringify(parameters),
-        headers: { 'content-type': 'application/json' },
-      },
-    );
-
-    expect(result).toBeDefined();
-    expect(result.id).toBeDefined();
-    expect(result.id).toEqual(executionId);
   });
 });
