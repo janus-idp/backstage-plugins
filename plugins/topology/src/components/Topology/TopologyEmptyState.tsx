9d8f244ae136cdf1980a5abf416180bce3f235ea
Patrick Knight <pknight@redhat.com>
2024-05-21 11:46:05 -0400

    feat(topology): add permissions to topology plugin (#1665)
    * feat(topology): add permissions to topology plugin
    
    * feat(topology): Update topology empty state
    
    * feat(topology): small doc update

--- plugins/topology/src/components/Topology/TopologyEmptyState.tsx
+++ plugins/topology/src/components/Topology/TopologyEmptyState.tsx
@@ -2,13 +2,22 @@ import React from 'react';
 
 import {
   EmptyState,
+  EmptyStateBody,
   EmptyStateHeader,
   EmptyStateIcon,
   EmptyStateVariant,
 } from '@patternfly/react-core';
 import { TopologyIcon } from '@patternfly/react-icons';
 
-export const TopologyEmptyState = () => {
+type TopologyEmptyStateProps = {
+  title?: string;
+  description?: string;
+};
+
+export const TopologyEmptyState = ({
+  title,
+  description,
+}: TopologyEmptyStateProps) => {
   return (
     <EmptyState
       variant={EmptyStateVariant.full}
@@ -16,10 +25,12 @@ export const TopologyEmptyState = () => {
       className="pf-topology-visualization-surface"
     >
       <EmptyStateHeader
-        titleText="No resources found"
+        titleText={title || 'No resources found'}
         icon={<EmptyStateIcon icon={TopologyIcon} />}
         headingLevel="h3"
-      />
+      >
+        <EmptyStateBody>{description}</EmptyStateBody>
+      </EmptyStateHeader>
     </EmptyState>
   );
 };
