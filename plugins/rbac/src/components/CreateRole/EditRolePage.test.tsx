c73440bc35f6e14b5aea1cc632cebae0b4627980
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2024-05-22 20:12:45 +0530

    fix(rbac): fix to enable create and edit role buttons on having correct permissions

--- plugins/rbac/src/components/CreateRole/EditRolePage.test.tsx
+++ plugins/rbac/src/components/CreateRole/EditRolePage.test.tsx
@@ -47,17 +47,6 @@ jest.mock('./RoleForm', () => ({
   RoleForm: () => <div>RoleForm</div>,
 }));
 
-jest.mock('@backstage/plugin-permission-react', () => ({
-  RequirePermission: jest
-    .fn()
-    .mockImplementation(({ permission, resourceRef, children }) => (
-      <div>
-        {`${permission} ${resourceRef}`}
-        {children}
-      </div>
-    )),
-}));
-
 jest.mock('@backstage/core-components', () => ({
   useQueryParamState: () => ['roleName', jest.fn()],
   Progress: () => <div>MockedProgressComponent</div>,
@@ -117,6 +106,7 @@ describe('EditRolePage', () => {
       loading: false,
       roleError: { name: '', message: '' },
       membersError: new Error(''),
+      canReadUsersAndGroups: true,
     });
     render(<EditRolePage />);
     expect(screen.getByText('Edit Role Page')).toBeInTheDocument();
@@ -131,6 +121,7 @@ describe('EditRolePage', () => {
       role: undefined,
       membersError: { name: '', message: '' },
       roleError: { name: '', message: '' },
+      canReadUsersAndGroups: true,
     });
     render(<EditRolePage />);
     expect(screen.queryByText('MockedProgressComponent')).toBeInTheDocument();
@@ -144,6 +135,7 @@ describe('EditRolePage', () => {
       role: undefined,
       membersError: { name: 'Error', message: 'Error Message' },
       loading: false,
+      canReadUsersAndGroups: true,
     });
     render(<EditRolePage />);
     expect(screen.queryByText('MockedErrorPageComponent')).toBeInTheDocument();
