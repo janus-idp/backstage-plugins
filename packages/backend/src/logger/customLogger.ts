a14ab842e2f8f64dd2c4182658a74518029b346b
Oleksandr Andriienko <oandriie@redhat.com>
2024-06-04 16:31:43 +0300

    Update packages/backend/src/logger/customLogger.ts
    Co-authored-by: Paul Schultz <pschultz@pobox.com>

--- packages/backend/src/logger/customLogger.ts
+++ packages/backend/src/logger/customLogger.ts
@@ -25,9 +25,11 @@ const auditLogFormat = winston.format((info, opts) => {
   const { isAuditLog, ...newInfo } = info;
 
   if (isAuditLog) {
-    return opts.isAuditLog ? newInfo : false;
+    // keep `isAuditLog` field
+    return opts.isAuditLog ? info : false;
   }
 
+  // remove `isAuditLog` field from non audit log events
   return !opts.isAuditLog ? newInfo : false;
 });
 
