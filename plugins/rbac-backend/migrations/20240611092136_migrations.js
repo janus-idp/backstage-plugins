eec08897f07dcb185a2a038b49825b31422b964a
Oleksandr Andriienko <oandriie@redhat.com>
2024-06-27 09:56:29 +0300

    fix(rbac): fix migration script
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- plugins/rbac-backend/migrations/20240611092136_migrations.js
+++ plugins/rbac-backend/migrations/20240611092136_migrations.js
@@ -5,7 +5,9 @@
 exports.up = async function up(knex) {
   const policyMetadataExist = await knex.schema.hasTable('policy-metadata');
 
-  await knex.schema.dropTable('policy-metadata');
+  if (policyMetadataExist) {
+    await knex.schema.dropTable('policy-metadata');
+  }
 };
 
 /**
