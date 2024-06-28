e33386a54c41078695d15d4ac7e5bd0021fca9c2
karthik <karthik.jk11@gmail.com>
2024-06-28 17:40:57 +0530

    feat(tekton): add permissions support for tekton plugin

--- plugins/tekton/src/components/Tekton/TektonCIComponent.tsx
+++ plugins/tekton/src/components/Tekton/TektonCIComponent.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 import { TektonResourcesContext } from '../../hooks/TektonResourcesContext';
 import { useDarkTheme } from '../../hooks/useDarkTheme';
 import { useTektonObjectsResponse } from '../../hooks/useTektonObjectsResponse';
+import { useTektonViewPermission } from '../../hooks/useTektonViewPermission';
 import { ModelsPlural } from '../../models';
+import PermissionAlert from '../common/PermissionAlert';
 import PipelineRunList from '../PipelineRunList/PipelineRunList';
 
 export const TektonCIComponent = () => {
@@ -15,7 +17,11 @@ export const TektonCIComponent = () => {
     ModelsPlural.pods,
   ];
   const tektonResourcesContextData = useTektonObjectsResponse(watchedResources);
+  const hasViewPermission = useTektonViewPermission();
 
+  if (!hasViewPermission) {
+    return <PermissionAlert />;
+  }
   return (
     <TektonResourcesContext.Provider value={tektonResourcesContextData}>
       <PipelineRunList />
