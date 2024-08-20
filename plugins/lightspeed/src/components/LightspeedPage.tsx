8ca0adf37e1acf6de18ab98dd25dedd050f89229
Divyanshi Gupta <divyanshigupta3295@gmail.com>
2024-08-20 20:02:57 +0530

    feat(lightspeed): add support to select AI models runnning on LLM server

--- plugins/lightspeed/src/components/LightspeedPage.tsx
+++ plugins/lightspeed/src/components/LightspeedPage.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import { useAsync } from 'react-use';
 
-import { Content, Header, HeaderLabel, Page } from '@backstage/core-components';
+import { Content, Header, Page, Select } from '@backstage/core-components';
 import { useApi } from '@backstage/core-plugin-api';
 
-import { Paper } from '@material-ui/core';
+import { LinearProgress, Paper } from '@material-ui/core';
 import { createStyles, makeStyles } from '@material-ui/core/styles';
 
 import { lightspeedApiRef } from '../api/LightspeedProxyClient';
-import { LightspeedInput } from './LightspeedInput';
-import { SystemMessage, UserMessage } from './Message';
+import { LightspeedChatBox } from './LightspeedChatBox';
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -40,48 +40,19 @@ export const LightspeedPage = () => {
 
   const lightspeedApi = useApi(lightspeedApiRef);
 
-  const [, setChunkIndex] = React.useState(0);
-  const [convoIndex, setConvoIndex] = React.useState<number[]>([0]);
-  const [messages, setMessages] = React.useState<{
-    [key: string]: { user: string; assistant: string };
-  }>({});
-
-  const convoCounter = convoIndex.length - 1;
-
-  const context_template = Object.values(messages)
-    .map(convo => `USER: ${convo.user}\nASSISTANT: ${convo.assistant}`)
-    .join('\n\n');
-
-  const handleInputPrompt = React.useCallback(
-    async (prompt: string) => {
-      setChunkIndex(0);
-      setMessages(m => {
-        m[convoCounter] = {
-          user: prompt,
-          assistant: '',
-        };
-        return m;
-      });
-
-      const latestQuestion = `Respond to the users current message\n\nCONTEXT: ${context_template}\n\nCURRENT QUESTION: ${prompt}`;
-
-      const result = await lightspeedApi.createChatCompletions(
-        `${latestQuestion}`,
-      );
-      for await (const chunk of result) {
-        setChunkIndex(index => index + 1);
-        setMessages(m => {
-          m[convoCounter] = {
-            ...m[convoCounter],
-            assistant: `${m[`${convoCounter}`]?.assistant || ''}${chunk.choices[0]?.delta?.content || ''}`,
-          };
-          return m;
-        });
-      }
-      setConvoIndex(conIndex => [...conIndex, convoCounter + 1]);
-    },
-    [context_template, convoCounter, lightspeedApi],
+  const { value: models, loading } = useAsync(
+    async () => await lightspeedApi.getAllModels(),
   );
+  const [selectedModel, setSelectedModel] = React.useState('');
+
+  const modelsItems = React.useMemo(
+    () => (models ? models.map(m => ({ label: m.id, value: m.id })) : []),
+    [models],
+  );
+
+  React.useEffect(() => {
+    if (modelsItems.length > 0) setSelectedModel(modelsItems[0].value);
+  }, [modelsItems]);
 
   return (
     <Page themeId="tool">
@@ -89,24 +60,21 @@ export const LightspeedPage = () => {
         title="Red Hat Developer Hub Lightspeed"
         subtitle="A new way to interact with LLMs inside Developer Hub."
       >
-        <HeaderLabel label="Owner" value="Team X" />
-        <HeaderLabel label="Lifecycle" value="Alpha" />
+        {loading && <LinearProgress />}
+        {!loading && models && (
+          <Select
+            onChange={item => setSelectedModel(item as string)}
+            items={modelsItems}
+            selected={selectedModel}
+            disabled={loading}
+            margin="dense"
+            label="Select LLM Model"
+          />
+        )}
       </Header>
       <Content className={classes.container}>
         <Paper className={classes.paper} elevation={2}>
-          <Paper id="style-1" className={classes.messagesBody}>
-            {convoIndex.map(id => (
-              <>
-                {messages[id]?.user && (
-                  <UserMessage message={messages[id].user} />
-                )}
-                {messages[id]?.assistant && (
-                  <SystemMessage message={messages[id].assistant} />
-                )}
-              </>
-            ))}
-          </Paper>
-          <LightspeedInput onSubmit={handleInputPrompt} />
+          <LightspeedChatBox selectedModel={selectedModel} />
         </Paper>
       </Content>
     </Page>
