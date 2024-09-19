feae7008893bcf78f45533c1d1796d99d37320c2
Dominika Zemanovicova <dzemanov@redhat.com>
2024-09-19 16:18:28 +0200

    remove legacy plugin installer
    Signed-off-by: Dominika Zemanovicova <dzemanov@redhat.com>

--- plugins/quay-actions/src/dynamic/index.ts
+++
@@ -1,8 +0,0 @@
-import { BackendDynamicPluginInstaller } from '@backstage/backend-dynamic-feature-service';
-
-import { createQuayRepositoryAction } from '../actions';
-
-export const dynamicPluginInstaller: BackendDynamicPluginInstaller = {
-  kind: 'legacy',
-  scaffolder: () => [createQuayRepositoryAction()],
-};
