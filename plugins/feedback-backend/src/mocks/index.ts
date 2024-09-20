e417722934cf0b66c8f863dab2e996855e76d03c
Nick Boldt <nboldt@redhat.com>
2024-09-19 23:26:40 -0300

    regen yarnlock
    Signed-off-by: Nick Boldt <nboldt@redhat.com>
    
    chore(deprecation/removal): remove feedback, feedback-backend, and catalog-backend-module-scaffolder-relation-processor as these have moved to BCP repo
    
    Signed-off-by: Nick Boldt <nboldt@redhat.com>
    
    regen yarnlock
    
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- plugins/feedback-backend/src/mocks/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './feedback';
-export * from './entity';
-export * from './user';
-export * from './config';
-export * from './jira';
--- plugins/feedback/src/mocks/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './entity';
-export * from './feedback';
-export * from './jiraDetails';
