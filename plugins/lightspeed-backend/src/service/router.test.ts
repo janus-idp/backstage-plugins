ef9ee685be72508eb6c5b174da2565076515cb56
Stephanie Cao <yangcao@redhat.com>
2024-09-21 02:03:12 -0400

    feat(lightspeed): add backend config and /conversations/:conversation_id GET&DELETE API (#2211)
    * add backend config and load session API and tests
    
    Signed-off-by: Stephanie <yangcao@redhat.com>
    
    * generate dist-dynamic
    
    Signed-off-by: Stephanie <yangcao@redhat.com>
    
    * fix tsc
    
    Signed-off-by: Stephanie <yangcao@redhat.com>
    
    * update to conversations endpoint with path params
    
    Signed-off-by: Stephanie <yangcao@redhat.com>
    
    * run yarn tsc
    
    Signed-off-by: Stephanie <yangcao@redhat.com>
    
    * add delete API
    
    Signed-off-by: Stephanie <yangcao@redhat.com>
    
    * resolve review comments
    
    Signed-off-by: Stephanie <yangcao@redhat.com>
    
    ---------
    
    Signed-off-by: Stephanie <yangcao@redhat.com>

--- plugins/lightspeed-backend/src/service/router.test.ts
+++ plugins/lightspeed-backend/src/service/router.test.ts
@@ -1,11 +1,13 @@
 import { getVoidLogger } from '@backstage/backend-common';
 // import { mockCredentials, mockServices } from '@backstage/backend-test-utils';
-import { ConfigReader } from '@backstage/config';
+import { MockConfigApi } from '@backstage/test-utils';
 
 import { AIMessage } from '@langchain/core/messages';
 import express from 'express';
 import request from 'supertest';
 
+import { saveHistory } from '../handlers/chatHistory';
+import { Roles } from '../service/types';
 import { createRouter } from './router';
 
 const mockAIMessage = new AIMessage('Mockup AI Message');
@@ -27,6 +29,22 @@ jest.mock('@langchain/core/prompts', () => {
   };
 });
 
+const mockConversationId = 'user1+1q2w3e4r-qwer1234';
+const mockServerURL = 'http://localhost:7007/api/proxy/lightspeed/api';
+const mockModel = 'test-model';
+
+const mockConfiguration = new MockConfigApi({
+  lightspeed: {
+    servers: [
+      {
+        id: 'test-server',
+        url: mockServerURL,
+        token: 'dummy-token',
+      },
+    ],
+  },
+});
+
 (global.fetch as jest.Mock) = jest.fn();
 
 describe('createRouter', () => {
@@ -35,7 +53,7 @@ describe('createRouter', () => {
   beforeAll(async () => {
     const router = await createRouter({
       logger: getVoidLogger(),
-      config: new ConfigReader({}),
+      config: mockConfiguration,
 
       // TODO: for user authentication
       // httpAuth: mockServices.httpAuth({
@@ -62,9 +80,47 @@ describe('createRouter', () => {
     });
   });
 
-  const mockConversationId = 'user1+1q2w3e4r-qwer1234';
-  const mockServerURL = 'http://localhost:7007/api/proxy/lightspeed/api';
-  const mockModel = 'test-model';
+  describe('GET and DELETE /conversations/:conversation_id', () => {
+    it('load history', async () => {
+      const humanMessage = 'Hello';
+      const aiMessage = 'Hi! How can I help you today?';
+      await saveHistory(mockConversationId, Roles.HumanRole, humanMessage);
+      await saveHistory(mockConversationId, Roles.AIRole, aiMessage);
+
+      const response = await request(app).get(
+        `/conversations/${mockConversationId}`,
+      );
+      expect(response.statusCode).toEqual(200);
+      // Parse response body
+      const responseData = response.body;
+
+      // Check that responseData is an array
+      expect(Array.isArray(responseData)).toBe(true);
+      expect(responseData.length).toBe(2);
+
+      expect(responseData[0].id).toContain('HumanMessage');
+      expect(responseData[0].kwargs?.content).toBe(humanMessage);
+
+      expect(responseData[1].id).toContain('AIMessage');
+      expect(responseData[1].kwargs?.content).toBe(aiMessage);
+    });
+
+    it('delete history', async () => {
+      // delete request
+      const deleteResponse = await request(app).delete(
+        `/conversations/${mockConversationId}`,
+      );
+      expect(deleteResponse.statusCode).toEqual(200);
+    });
+
+    it('load history with deleted conversation_id', async () => {
+      const response = await request(app).get(
+        `/conversations/${mockConversationId}`,
+      );
+      expect(response.statusCode).toEqual(500);
+      expect(response.body.error).toContain('unknown conversation_id');
+    });
+  });
 
   describe('POST /v1/query', () => {
     it('chat completions', async () => {
--- plugins/lightspeed-backend/src/service/router.ts
+++ plugins/lightspeed-backend/src/service/router.ts
@@ -9,13 +9,26 @@ import { ChatOpenAI } from '@langchain/openai';
 import express from 'express';
 import Router from 'express-promise-router';
 
-import { QueryRequestBody, RouterOptions } from './types';
-import { validateCompletionsRequest } from './validation';
+import {
+  deleteHistory,
+  loadHistory,
+  saveHistory,
+} from '../handlers/chatHistory';
+import {
+  DEFAULT_HISTORY_LENGTH,
+  QueryRequestBody,
+  Roles,
+  RouterOptions,
+} from './types';
+import {
+  validateCompletionsRequest,
+  validateLoadHistoryRequest,
+} from './validation';
 
 export async function createRouter(
   options: RouterOptions,
 ): Promise<express.Router> {
-  const { logger } = options;
+  const { logger, config } = options;
 
   const router = Router();
   router.use(express.json());
@@ -24,6 +37,41 @@ export async function createRouter(
     response.json({ status: 'ok' });
   });
 
+  router.get(
+    '/conversations/:conversation_id',
+    validateLoadHistoryRequest,
+    async (request, response) => {
+      const conversation_id = request.params.conversation_id;
+      const historyLength = Number(request.query.historyLength);
+
+      const loadhistoryLength: number = historyLength || DEFAULT_HISTORY_LENGTH;
+      try {
+        const history = await loadHistory(conversation_id, loadhistoryLength);
+        response.status(200).json(history);
+        response.end();
+      } catch (error) {
+        const errormsg = `Error: ${error}`;
+        logger.error(errormsg);
+        response.status(500).json({ error: errormsg });
+      }
+    },
+  );
+
+  router.delete(
+    '/conversations/:conversation_id',
+    async (request, response) => {
+      const conversation_id = request.params.conversation_id;
+      try {
+        response.status(200).json(await deleteHistory(conversation_id));
+        response.end();
+      } catch (error) {
+        const errormsg = `${error}`;
+        logger.error(errormsg);
+        response.status(500).json({ error: errormsg });
+      }
+    },
+  );
+
   router.post(
     '/v1/query',
     validateCompletionsRequest,
@@ -31,18 +79,24 @@ export async function createRouter(
       const { conversation_id, model, query, serverURL }: QueryRequestBody =
         request.body;
       try {
+        // currently only supports single server
+        const apiToken = config
+          .getConfigArray('lightspeed.servers')[0]
+          .getOptionalString('token');
+
         const openAIApi = new ChatOpenAI({
-          apiKey: 'sk-no-key-required', // authorization token is used
+          apiKey: apiToken || 'sk-no-key-required', // set to sk-no-key-required if api token is not provided
           model: model,
           streaming: false,
+          // streaming: true,
+          // streamUsage: false,
           temperature: 0,
           configuration: {
-            // bearer token should already applied in proxy header
-            // baseOptions: {
-            //   headers: {
-            //     ...(token && { Authorization: `Bearer <token>` }),
-            //   },
-            // },
+            baseOptions: {
+              headers: {
+                ...(apiToken && { Authorization: `Bearer ${apiToken}` }),
+              },
+            },
             baseURL: serverURL,
           },
         });
@@ -60,12 +114,16 @@ export async function createRouter(
         const res = await chain.invoke({
           messages: [new HumanMessage(query)],
         });
+
         const data = {
           conversation_id: conversation_id,
           response: res.content,
         };
         response.json(data);
         response.end();
+
+        await saveHistory(conversation_id, Roles.HumanRole, query);
+        await saveHistory(conversation_id, Roles.AIRole, String(res.content));
       } catch (error) {
         const errormsg = `Error fetching completions from ${serverURL}: ${error}`;
         logger.error(errormsg);
