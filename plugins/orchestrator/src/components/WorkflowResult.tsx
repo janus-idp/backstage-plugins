3015f78d4dfb76379d123c64fa49f4420b942c53
Marek Libra <marek.libra@gmail.com>
2024-09-22 13:11:58 +0200

    Addapt to workflowdata.result

--- plugins/orchestrator/src/components/WorkflowResult.tsx
+++ plugins/orchestrator/src/components/WorkflowResult.tsx
@@ -5,7 +5,6 @@ import { RouteFunc, useApi, useRouteRef } from '@backstage/core-plugin-api';
 import { AboutField } from '@backstage/plugin-catalog';
 
 import {
-  Chip,
   CircularProgress,
   Grid,
   List,
@@ -13,16 +12,17 @@ import {
   makeStyles,
   Typography,
 } from '@material-ui/core';
+import DotIcon from '@material-ui/icons/FiberManualRecord';
 // TODO: use material-ui Stack once the library gets updated
 import { Stack, StackItem } from '@patternfly/react-core';
 
 import {
   AssessedProcessInstanceDTO,
-  capitalize,
-  parseWorkflowVariables,
+  ProcessInstanceErrorDTO,
   ProcessInstanceStatusDTO,
   QUERY_PARAM_ASSESSMENT_INSTANCE_ID,
   WorkflowOverviewDTO,
+  WorkflowResultDTO,
 } from '@janus-idp/backstage-plugin-orchestrator-common';
 
 import { orchestratorApiRef } from '../api';
@@ -30,145 +30,36 @@ import { VALUE_UNAVAILABLE } from '../constants';
 import { executeWorkflowRouteRef } from '../routes';
 import { buildUrl } from '../utils/UrlUtils';
 import { WorkflowDescriptionModal } from './WorkflowDescriptionModal';
-import { mapProcessInstanceToDetails } from './WorkflowInstancePageContent';
-import { WorkflowRunDetail, WorkflowSuggestion } from './WorkflowRunDetail';
-import { WorkflowVariablesViewer } from './WorkflowVariablesViewer';
 
-type NextWorkflowType = {
-  title: string;
-  link: string;
-  id: string;
-  isRecommended: boolean;
-};
-
-const useStyles = makeStyles(_ => ({
-  recommendedLabelContainer: {
-    display: 'flex',
-    alignItems: 'center',
-    whiteSpace: 'nowrap',
+const useStyles = makeStyles(theme => ({
+  outputGrid: {
+    '& h2': {
+      textTransform: 'none',
+      fontSize: 'small',
+    },
   },
-  recommendedLabel: { margin: '0 0.25rem' },
+  errorIcon: {
+    color: theme.palette.error.main,
+  },
 }));
-
-const getNextWorkflows = (
-  details: WorkflowRunDetail,
-  executeWorkflowLink: RouteFunc<{ workflowId: string }>,
-): NextWorkflowType[] => {
-  const nextWorkflows: {
-    title: string;
-    link: string;
-    id: string;
-    isRecommended: boolean;
-  }[] = [];
-
-  if (details.nextWorkflowSuggestions) {
-    Object.entries(details.nextWorkflowSuggestions).forEach(([_, value]) => {
-      const nextWorkflowSuggestions: WorkflowSuggestion[] = Array.isArray(value)
-        ? value
-        : [value];
-      nextWorkflowSuggestions.forEach(nextWorkflowSuggestion => {
-        // Produce flat structure
-        const routeUrl = executeWorkflowLink({
-          workflowId: nextWorkflowSuggestion.id,
-        });
-        const urlToNavigate = buildUrl(routeUrl, {
-          [QUERY_PARAM_ASSESSMENT_INSTANCE_ID]: details.id,
-        });
-        nextWorkflows.push({
-          title: nextWorkflowSuggestion.name,
-          link: urlToNavigate,
-          id: nextWorkflowSuggestion.id,
-          isRecommended:
-            (
-              details.nextWorkflowSuggestions
-                ?.currentVersion as WorkflowSuggestion
-            )?.id === nextWorkflowSuggestion.id,
-        });
-      });
-    });
-  }
-
-  return nextWorkflows;
-};
-
-/* DO NOT MERGE
-  These mocks will be removed once the new WorkflowResult schema lends and the API gets updated.
-  https://github.com/parodos-dev/serverless-workflows/pull/403
-*/
-const mockErrorInstance = {
-  id: 'my-workflow-instance-id',
-  state: 'Error',
-  error: 'This is an error message by workflow.',
-  results: {
-    // Of WorkflowResults type
-    message: 'A human-friendly description of an error.',
-    outputs: [
-      {
-        key: 'A resource name',
-        value: 'foo',
-        // default format
-      },
-      {
-        key: 'Another variable',
-        value: 'https://foo.bar',
-        format: 'link',
-      },
-      {
-        key: 'A numeric value',
-        value: 123,
-      },
-    ],
-  },
-};
-
-const mockSuccessInstance = {
-  id: 'my-workflow-instance-id',
-  state: 'Completed',
-  error: null,
-  results: {
-    // Of WorkflowResults type
-    message: 'A human-friendly description of recent state.',
-    nextWorkflows: [
-      {
-        id: '123-123-123',
-        name: 'Next suggested workflow',
-      },
-      {
-        id: '456-456-456',
-        name: 'Another option for next workflow',
-      },
-    ],
-    outputs: [
-      {
-        key: 'A resource name',
-        value: 'foo',
-        // default format
-      },
-      {
-        key: 'A link',
-        value: 'https://foo.bar',
-        format: 'link',
-      },
-      {
-        key: 'A numeric value',
-        value: 123,
-      },
-    ],
-  },
-};
 
 const ResultMessage = ({
   status,
   error,
   resultMessage,
+  completedWith,
 }: {
-  status: ProcessInstanceStatusDTO;
-  error?: string | null;
-  resultMessage?: string;
+  status?: ProcessInstanceStatusDTO;
+  error?: ProcessInstanceErrorDTO;
+  resultMessage?: WorkflowResultDTO['message'];
+  completedWith: WorkflowResultDTO['completedWith'];
 }) => {
+  const styles = useStyles();
+
+  const errorMessage = error?.message || error?.toString();
   let noResult = <></>;
-  if (!resultMessage && !error) {
-    if (['Error', 'Completed', 'Aborted', 'Suspended'].includes(status)) {
+  if (!resultMessage && !errorMessage) {
+    if (['Error', 'Completed', 'Aborted', 'Suspended'].includes(status || '')) {
       noResult = (
         <StackItem>
           <i>The workflow provided no additional info about the status.</i>
@@ -187,12 +78,23 @@ const ResultMessage = ({
 
   return (
     <>
-      {error && (
+      {resultMessage && (
         <StackItem>
-          <b>{error}</b>
+          {completedWith === 'error' && (
+            <DotIcon
+              style={{ fontSize: '0.75rem' }}
+              className={styles.errorIcon}
+            />
+          )}
+          &nbsp;
+          {resultMessage}
         </StackItem>
       )}
-      {resultMessage && <StackItem>{resultMessage}</StackItem>}
+      {errorMessage && (
+        <StackItem>
+          <b>{errorMessage}</b>
+        </StackItem>
+      )}
       {noResult}
     </>
   );
@@ -203,11 +105,7 @@ const NextWorkflows = ({
   nextWorkflows,
 }: {
   instanceId: string;
-  nextWorkflows: {
-    // TODO: use the generated type instead (not yet available)
-    id: string;
-    name: string;
-  }[];
+  nextWorkflows: WorkflowResultDTO['nextWorkflows'];
 }) => {
   const orchestratorApi = useApi(orchestratorApiRef);
   const executeWorkflowLink: RouteFunc<{ workflowId: string }> = useRouteRef(
@@ -305,13 +203,10 @@ const NextWorkflows = ({
 const WorkflowOutputs = ({
   outputs,
 }: {
-  // TODO: use the generated type instead (not yet available)
-  outputs?: {
-    key: string;
-    value: string | number;
-    format?: string;
-  }[];
+  outputs: WorkflowResultDTO['outputs'];
 }) => {
+  const styles = useStyles();
+
   if (!outputs?.length) {
     return null;
   }
@@ -321,35 +216,20 @@ const WorkflowOutputs = ({
       <Typography variant="h6" component="div">
         Output
       </Typography>
-      {!outputs?.length && (
-        <Typography variant="body1" component="div">
-          The workflow produced no additional data.
-        </Typography>
-      )}
       <Grid container alignContent="flex-start">
         {outputs?.map(item => {
+          // Keep the order set by backend, it might matter
           let value;
-          if (item.value) {
-            if (
-              !item.format ||
-              item.format === 'string' ||
-              item.format === 'number'
-            ) {
-              value = <b>{item.value}</b>;
-            } else if (item.format === 'link') {
-              value = <Link to={item.value as string}>{item.value}</Link>;
-            }
+          const itemValue = (item.value || VALUE_UNAVAILABLE) as string;
+          if (item.format === 'link') {
+            value = <Link to={itemValue}>{item.key}</Link>;
           } else {
-            value = VALUE_UNAVAILABLE;
+            value = <AboutField label={item.key} value={itemValue} />;
           }
 
           return (
-            <Grid item md={4} key={item.key}>
-              <AboutField label={item.key}>
-                <Typography variant="subtitle2" component="div">
-                  {value}
-                </Typography>
-              </AboutField>
+            <Grid item md={4} key={item.key} className={styles.outputGrid}>
+              {value}
             </Grid>
           );
         })}
@@ -361,23 +241,22 @@ const WorkflowOutputs = ({
 export const WorkflowResult: React.FC<{
   assessedInstance: AssessedProcessInstanceDTO;
 }> = ({ assessedInstance }) => {
-  // TODO: use the assessedInstance property instead
-  console.info('--- do not merge, mocking data for development');
-  // const instance = mockErrorInstance;
-  const instance = mockSuccessInstance;
+  const instance = assessedInstance.instance;
+  const result = instance.workflowdata?.result;
 
   return (
     <Stack hasGutter>
       <ResultMessage
-        status={instance.state}
+        status={instance.status}
         error={instance.error}
-        resultMessage={instance.results?.message}
+        resultMessage={result?.message}
+        completedWith={result?.completedWith}
       />
       <NextWorkflows
         instanceId={instance.id}
-        nextWorkflows={instance.results?.nextWorkflows}
+        nextWorkflows={result?.nextWorkflows}
       />
-      <WorkflowOutputs outputs={instance.results?.outputs} />
+      <WorkflowOutputs outputs={result?.outputs} />
     </Stack>
   );
 };
