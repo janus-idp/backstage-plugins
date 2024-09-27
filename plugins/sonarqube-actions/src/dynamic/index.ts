de1859db785399a67e6e759f6f3642eec7fb2760
Oleksandr Andriienko <oandriie@redhat.com>
2024-09-27 17:58:24 +0300

    feat(sonarqube-actions)!: migrate to the new Backend System and remove deprecations (#2259)
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- plugins/sonarqube-actions/src/dynamic/index.ts
+++
@@ -1,8 +0,0 @@
-import { BackendDynamicPluginInstaller } from '@backstage/backend-dynamic-feature-service';
-
-import { createSonarQubeProjectAction } from '../actions';
-
-export const dynamicPluginInstaller: BackendDynamicPluginInstaller = {
-  kind: 'legacy',
-  scaffolder: () => [createSonarQubeProjectAction()],
-};
