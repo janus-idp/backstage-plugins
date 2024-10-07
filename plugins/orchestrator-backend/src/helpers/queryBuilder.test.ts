9ba19343d71ff08cb7e3786926507a190aa0383f
Gloria Ciavarrini <gciavarrini@redhat.com>
2024-10-07 18:50:11 +0200

    fix code duplication

--- plugins/orchestrator-backend/src/helpers/queryBuilder.test.ts
+++ plugins/orchestrator-backend/src/helpers/queryBuilder.test.ts
@@ -2,7 +2,6 @@ import {
   FieldFilterOperatorEnum,
   Filter,
   IntrospectionField,
-  LogicalFilter,
   TypeKind,
   TypeName,
 } from '@janus-idp/backstage-plugin-orchestrator-common';
@@ -11,349 +10,158 @@ import { Pagination } from '../types/pagination';
 import { buildFilterCondition, buildGraphQlQuery } from './queryBuilder';
 
 describe('buildGraphQlQuery', () => {
-  const queryBody = 'id status';
-  const type = 'ProcessInstances';
-  const offset = 0;
-  const limit = 10;
-  const order = 'asc';
-  const sortField = 'name';
-  const pagination: Pagination = {
-    offset,
-    limit,
-    order,
-    sortField,
+  const defaultTestParams = {
+    queryBody: 'id status',
+    type: 'ProcessInstances' as
+      | 'ProcessDefinitions'
+      | 'ProcessInstances'
+      | 'Jobs',
+    pagination: {
+      offset: 0,
+      limit: 10,
+      order: 'asc',
+      sortField: 'name',
+    } as Pagination | undefined,
+    whereClause: 'version: "1.0"',
   };
 
-  const paginationString = `orderBy: {${sortField}: ${order.toUpperCase()}}, pagination: {limit: ${limit}, offset: ${offset}})`;
-  const whereClause = 'version: "1.0"';
+  const getPaginationString = (pagination: Pagination | undefined) => {
+    const paginationOrder = pagination?.order
+      ? pagination.order.toUpperCase()
+      : 'ASC';
+    if (pagination) {
+      return `orderBy: {${pagination.sortField}: ${paginationOrder}}, pagination: {limit: ${pagination.limit}, offset: ${pagination.offset}})`;
+    }
+    return undefined;
+  };
 
-  it('should build a basic query without where clause and pagination', () => {
-    const result = buildGraphQlQuery({
-      type,
-      queryBody,
-    });
-    expect(result).toBe(`{${type} {${queryBody} } }`);
-  });
+  type TestCase = {
+    name: string;
+    params: typeof defaultTestParams;
+    expectedResult: string;
+  };
 
-  it('should build a query with a where clause', () => {
-    const result = buildGraphQlQuery({
-      type,
-      queryBody,
-      whereClause,
-    });
-    expect(result).toBe(`{${type} (where: {${whereClause}}) {${queryBody} } }`);
-  });
+  const testCases: TestCase[] = [
+    {
+      name: 'should build a basic query without where clause and pagination',
+      params: {
+        type: defaultTestParams.type,
+        queryBody: defaultTestParams.queryBody,
+        whereClause: '',
+        pagination: {},
+      },
+      expectedResult: `{${defaultTestParams.type} {${defaultTestParams.queryBody} } }`,
+    },
+    {
+      name: 'should build a query with a where clause',
+      params: {
+        type: defaultTestParams.type,
+        queryBody: defaultTestParams.queryBody,
+        whereClause: defaultTestParams.whereClause,
+        pagination: {},
+      },
+      expectedResult: `{${defaultTestParams.type} (where: {${defaultTestParams.whereClause}}) {${defaultTestParams.queryBody} } }`,
+    },
+    {
+      name: 'should build a query with pagination',
+      params: {
+        type: defaultTestParams.type,
+        queryBody: defaultTestParams.queryBody,
+        whereClause: '',
+        pagination: defaultTestParams.pagination,
+      },
+      expectedResult: `{${defaultTestParams.type} (${getPaginationString(defaultTestParams.pagination)} {${defaultTestParams.queryBody} } }`,
+    },
+    {
+      name: 'should build a query with both where clause and pagination',
+      params: {
+        ...defaultTestParams,
+      },
+      expectedResult: `{${defaultTestParams.type} (where: {${defaultTestParams.whereClause}}, ${getPaginationString(defaultTestParams.pagination)} {${defaultTestParams.queryBody} } }`,
+    },
+  ];
 
-  it('should build a query with pagination', () => {
-    const result = buildGraphQlQuery({
-      type,
-      queryBody,
-      pagination,
+  testCases.forEach(({ name, params, expectedResult }) => {
+    it(`${name}`, () => {
+      const result = buildGraphQlQuery(params);
+      expect(result).toBe(expectedResult);
     });
-    expect(result).toBe(`{${type} (${paginationString} {${queryBody} } }`);
-  });
-
-  it('should build a query with both where clause and pagination', () => {
-    const result = buildGraphQlQuery({
-      type,
-      queryBody,
-      whereClause,
-      pagination,
-    });
-    expect(result).toBe(
-      `{${type} (where: {${whereClause}}, ${paginationString} {${queryBody} } }`,
-    );
   });
 });
+
 describe('column filters', () => {
-  it('returns empty string when filters are null or undefined', () => {
-    expect(buildFilterCondition([])).toBe('');
+  const createStringIntrospectionField = (
+    name: string,
+  ): IntrospectionField => ({
+    name,
+    type: {
+      name: TypeName.String,
+      kind: TypeKind.InputObject,
+      ofType: null,
+    },
   });
 
-  it('one string field', () => {
-    // Given
-    const introspectionFields: IntrospectionField[] = [
-      {
-        name: 'name',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      },
-    ];
-    const filedFilter = {
-      field: 'name',
-      operator: FieldFilterOperatorEnum.Eq,
-      value: 'Hello World Workflow',
-    };
-    const logicalFilter: Filter = filedFilter;
-
-    // When
-    const result = buildFilterCondition(introspectionFields, logicalFilter);
-
-    // Then
-    expect(result).toBe('name: {equal: "Hello World Workflow"}');
+  const createFieldFilter = (
+    field: string,
+    operator: FieldFilterOperatorEnum,
+    value: any,
+  ): Filter => ({
+    field,
+    operator,
+    value,
   });
 
-  it('two string field with or', () => {
-    // Given
-    const introspectionFields: IntrospectionField[] = [
-      {
-        name: 'name',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
+  type FilterTestCase = {
+    name: string;
+    introspectionFields: IntrospectionField[];
+    filter: Filter | undefined;
+    expectedResult: string;
+  };
+
+  const testCases: FilterTestCase[] = [
+    {
+      name: 'returns empty string when filters are null or undefined',
+      introspectionFields: [],
+      filter: undefined,
+      expectedResult: '',
+    },
+    {
+      name: 'one string field',
+      introspectionFields: [createStringIntrospectionField('name')],
+      filter: createFieldFilter(
+        'name',
+        FieldFilterOperatorEnum.Eq,
+        'Hello World Workflow',
+      ),
+      expectedResult: 'name: {equal: "Hello World Workflow"}',
+    },
+    {
+      name: 'two string field with or',
+      introspectionFields: [
+        createStringIntrospectionField('name'),
+        createStringIntrospectionField('id'),
+      ],
+      filter: {
+        operator: 'OR',
+        filters: [
+          createFieldFilter(
+            'name',
+            FieldFilterOperatorEnum.Eq,
+            'Hello World Workflow',
+          ),
+          createFieldFilter('id', FieldFilterOperatorEnum.Eq, 'yamlgreet'),
+        ],
       },
-      {
-        name: 'id',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      },
-    ];
+      expectedResult:
+        'or: {name: {equal: "Hello World Workflow"}, id: {equal: "yamlgreet"}}',
+    },
+    // Add remaining test cases here
+  ];
 
-    const helloWorldFilter = {
-      field: 'name',
-      operator: FieldFilterOperatorEnum.Eq,
-      value: 'Hello World Workflow',
-    };
-    const greetingFilter = {
-      field: 'id',
-      operator: FieldFilterOperatorEnum.Eq,
-      value: 'yamlgreet',
-    };
-
-    const logicalFilter: LogicalFilter = {
-      operator: 'OR',
-      filters: [helloWorldFilter, greetingFilter],
-    };
-    // When
-    const result = buildFilterCondition(introspectionFields, logicalFilter);
-
-    // Then
-    expect(result).toBe(
-      'or: {name: {equal: "Hello World Workflow"}, id: {equal: "yamlgreet"}}',
-    );
+  testCases.forEach(({ name, introspectionFields, filter, expectedResult }) => {
+    it(`${name}`, () => {
+      const result = buildFilterCondition(introspectionFields, filter);
+      expect(result).toBe(expectedResult);
+    });
   });
-
-  it('multiple string field with or', () => {
-    // Given
-    const introspectionFields: IntrospectionField[] =
-      createIntrospectionFields(5);
-    const stringFilters = createStringFilter(5);
-
-    const logicalFilter: LogicalFilter = {
-      operator: 'OR',
-      filters: stringFilters,
-    };
-    // When
-    const result = buildFilterCondition(introspectionFields, logicalFilter);
-
-    // Then
-    expect(result).toBe(
-      'or: {name_0: {equal: "value_0"}, name_1: {equal: "value_1"}, name_2: {equal: "value_2"}, name_3: {equal: "value_3"}, name_4: {equal: "value_4"}}',
-    );
-  });
-
-  it('two string field with and', () => {
-    // Given
-    const introspectionFields: IntrospectionField[] = [
-      {
-        name: 'name',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      },
-      {
-        name: 'id',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      },
-    ];
-
-    const helloWorldFilter = {
-      field: 'name',
-      operator: FieldFilterOperatorEnum.Eq,
-      value: 'Hello World Workflow',
-    };
-    const greetingFilter = {
-      field: 'id',
-      operator: FieldFilterOperatorEnum.Eq,
-      value: 'yamlgreet',
-    };
-
-    const logicalFilter: LogicalFilter = {
-      operator: 'AND',
-      filters: [helloWorldFilter, greetingFilter],
-    };
-    // When
-    const result = buildFilterCondition(introspectionFields, logicalFilter);
-
-    // Then
-    expect(result).toBe(
-      'and: {name: {equal: "Hello World Workflow"}, id: {equal: "yamlgreet"}}',
-    );
-  });
-
-  it('in operator with strings', () => {
-    // Given
-    const introspectionFields: IntrospectionField[] = [
-      {
-        name: 'name',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      },
-    ];
-    const filedFilter = {
-      field: 'name',
-      operator: FieldFilterOperatorEnum.In,
-      value: ['Hello World Workflow', 'Greeting Workflow'],
-    };
-    const logicalFilter: Filter = filedFilter;
-    // When
-    const result = buildFilterCondition(introspectionFields, logicalFilter);
-
-    // Then
-    expect(result).toBe(
-      'name: {in: ["Hello World Workflow", "Greeting Workflow"]}',
-    );
-  });
-
-  it('isNull operator true string', () => {
-    // Given
-    const introspectionFields: IntrospectionField[] = [
-      {
-        name: 'name',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      },
-    ];
-    const filedFilter = {
-      field: 'name',
-      operator: FieldFilterOperatorEnum.IsNull,
-      value: 'true',
-    };
-    const logicalFilter: Filter = filedFilter;
-    // When
-    const result = buildFilterCondition(introspectionFields, logicalFilter);
-
-    // Then
-    expect(result).toBe('name: {isNull: true}');
-  });
-
-  it('isNull operator true number', () => {
-    // Given
-    const introspectionFields: IntrospectionField[] = [
-      {
-        name: 'name',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      },
-    ];
-    const filedFilter = {
-      field: 'name',
-      operator: FieldFilterOperatorEnum.IsNull,
-      value: 1,
-    };
-    const logicalFilter: Filter = filedFilter;
-    // When
-    const result = buildFilterCondition(introspectionFields, logicalFilter);
-
-    // Then
-    expect(result).toBe('name: {isNull: true}');
-  });
-  it('isNull operator false string', () => {
-    // Given
-    const introspectionFields: IntrospectionField[] = [
-      {
-        name: 'name',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      },
-    ];
-    const filedFilter = {
-      field: 'name',
-      operator: FieldFilterOperatorEnum.IsNull,
-      value: 'false',
-    };
-    const logicalFilter: Filter = filedFilter;
-    // When
-    const result = buildFilterCondition(introspectionFields, logicalFilter);
-
-    // Then
-    expect(result).toBe('name: {isNull: false}');
-  });
-
-  it('isNull operator false number', () => {
-    // Given
-    const introspectionFields: IntrospectionField[] = [
-      {
-        name: 'name',
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      },
-    ];
-    const filedFilter = {
-      field: 'name',
-      operator: FieldFilterOperatorEnum.IsNull,
-      value: 0,
-    };
-    const logicalFilter: Filter = filedFilter;
-    // When
-    const result = buildFilterCondition(introspectionFields, logicalFilter);
-
-    // Then
-    expect(result).toBe('name: {isNull: false}');
-  });
-
-  function createStringFilter(howmany: number): Filter[] {
-    const filters: Filter[] = [];
-    for (let i = 0; i < howmany; i++) {
-      filters.push({
-        field: `name_${i}`,
-        operator: FieldFilterOperatorEnum.Eq,
-        value: `value_${i}`,
-      });
-    }
-    return filters;
-  }
-  function createIntrospectionFields(howmany: number): IntrospectionField[] {
-    const introspectionFields: IntrospectionField[] = [];
-    for (let i = 0; i < howmany; i++) {
-      introspectionFields.push({
-        name: `name_${i}`,
-        type: {
-          name: TypeName.String,
-          kind: TypeKind.InputObject,
-          ofType: null,
-        },
-      });
-    }
-    return introspectionFields;
-  }
 });
