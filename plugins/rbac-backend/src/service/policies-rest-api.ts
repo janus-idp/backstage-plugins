25166f63f5172dc22dd731be31733280a487cf98
Oleksandr Andriienko <oandriie@redhat.com>
2024-10-16 17:17:59 +0300

    fix(rbac): make working rbac-backend with newer APIs (#2359)
    * fix(rbac): make working rbac-backend with newer APIs
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>
    
    * fit(rbac): fix unit tests
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>
    
    * fix(rbac): fix lint
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>
    
    * fix(orchestrator): fix lint
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>
    
    * fix(rbac): update error type, when httpAuth is not availiable
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>
    
    ---------
    
    Signed-off-by: Oleksandr Andriienko <oandriie@redhat.com>

--- plugins/rbac-backend/src/service/policies-rest-api.ts
+++ plugins/rbac-backend/src/service/policies-rest-api.ts
@@ -9,8 +9,8 @@ import {
   InputError,
   NotAllowedError,
   NotFoundError,
+  ServiceUnavailableError,
 } from '@backstage/errors';
-import type { IdentityApi } from '@backstage/plugin-auth-node';
 import {
   createRouter,
   RouterOptions,
@@ -94,22 +94,26 @@ export class PoliciesServer {
 
   private async authorize(
     request: Request,
-    identity: IdentityApi,
     permission: ResourcePermission,
   ): Promise<PolicyDecision> {
-    if (permission !== policyEntityReadPermission) {
-      const userIdentity = await identity.getIdentity({ request });
-      if (!userIdentity) {
-        throw new NotAllowedError('User identity not found');
-      }
+    const credentials = await this.httpAuth.credentials(request, {
+      allow: ['user', 'service'],
+    });
+
+    // allow service to service communication, but only with read permission
+    if (
+      this.auth.isPrincipal(credentials, 'service') &&
+      permission !== policyEntityReadPermission
+    ) {
+      throw new NotAllowedError(
+        `Only creadential principal with type 'user' permitted to modify permissions`,
+      );
     }
 
     const decision = (
       await this.permissions.authorize(
         [{ permission: permission, resourceRef: permission.resourceType }],
-        {
-          credentials: await this.httpAuth.credentials(request),
-        },
+        { credentials },
       )
     )[0];
 
@@ -119,11 +123,11 @@ export class PoliciesServer {
   async serve(): Promise<express.Router> {
     const router = await createRouter(this.options);
 
-    const { identity } = this.options;
+    const { httpAuth } = this.options;
 
-    if (!identity) {
-      throw new NotAllowedError(
-        'Identity api not found, ensure the correct configuration for the RBAC plugin',
+    if (!httpAuth) {
+      throw new ServiceUnavailableError(
+        'httpAuth not found, ensure the correct configuration for the RBAC plugin',
       );
     }
 
@@ -142,7 +146,6 @@ export class PoliciesServer {
     router.get('/', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityReadPermission,
       );
 
@@ -157,7 +160,6 @@ export class PoliciesServer {
     router.get('/policies', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityReadPermission,
       );
 
@@ -197,7 +199,6 @@ export class PoliciesServer {
       async (request, response) => {
         const decision = await this.authorize(
           request,
-          identity,
           policyEntityReadPermission,
         );
 
@@ -232,7 +233,6 @@ export class PoliciesServer {
       async (request, response) => {
         const decision = await this.authorize(
           request,
-          identity,
           policyEntityDeletePermission,
         );
 
@@ -272,7 +272,6 @@ export class PoliciesServer {
     router.post('/policies', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityCreatePermission,
       );
 
@@ -314,7 +313,6 @@ export class PoliciesServer {
       async (request, response) => {
         const decision = await this.authorize(
           request,
-          identity,
           policyEntityUpdatePermission,
         );
 
@@ -402,7 +400,6 @@ export class PoliciesServer {
     router.get('/roles', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityReadPermission,
       );
 
@@ -429,7 +426,6 @@ export class PoliciesServer {
     router.get('/roles/:kind/:namespace/:name', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityReadPermission,
       );
 
@@ -465,7 +461,6 @@ export class PoliciesServer {
       const uniqueItems = new Set<string>();
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityCreatePermission,
       );
 
@@ -506,8 +501,10 @@ export class PoliciesServer {
         }
       }
 
-      const user = await identity.getIdentity({ request });
-      const modifiedBy = user?.identity.userEntityRef!;
+      const credentials = await httpAuth.credentials(request, {
+        allow: ['user'],
+      });
+      const modifiedBy = credentials.principal.userEntityRef;
       const metadata: RoleMetadataDao = {
         roleEntityRef: roleRaw.name,
         source: 'rest',
@@ -538,7 +535,6 @@ export class PoliciesServer {
       const uniqueItems = new Set<string>();
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityUpdatePermission,
       );
 
@@ -575,12 +571,15 @@ export class PoliciesServer {
       const newRole = this.transformRoleToArray(newRoleRaw);
       // todo shell we allow newRole with an empty array?...
 
-      const user = await identity.getIdentity({ request });
+      const credentials = await httpAuth.credentials(request, {
+        allow: ['user'],
+      });
+
       const newMetadata: RoleMetadataDao = {
         ...newRoleRaw.metadata,
         source: newRoleRaw.metadata?.source ?? 'rest',
         roleEntityRef: newRoleRaw.name,
-        modifiedBy: user?.identity.userEntityRef!,
+        modifiedBy: credentials.principal.userEntityRef,
       };
 
       const oldMetadata =
@@ -679,7 +678,6 @@ export class PoliciesServer {
       async (request, response) => {
         const decision = await this.authorize(
           request,
-          identity,
           policyEntityDeletePermission,
         );
 
@@ -726,11 +724,14 @@ export class PoliciesServer {
           throw new NotAllowedError(`Unable to delete role: ${err.message}`);
         }
 
-        const user = await identity.getIdentity({ request });
+        const credentials = await httpAuth.credentials(request, {
+          allow: ['user'],
+        });
+
         const metadata: RoleMetadataDao = {
           roleEntityRef,
           source: 'rest',
-          modifiedBy: user?.identity.userEntityRef!,
+          modifiedBy: credentials.principal.userEntityRef,
         };
 
         await this.enforcer.removeGroupingPolicies(
@@ -759,7 +760,6 @@ export class PoliciesServer {
     router.get('/plugins/policies', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityReadPermission,
       );
 
@@ -784,7 +784,6 @@ export class PoliciesServer {
     router.get('/plugins/condition-rules', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityReadPermission,
       );
 
@@ -811,7 +810,6 @@ export class PoliciesServer {
     router.get('/roles/conditions', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityReadPermission,
       );
 
@@ -849,7 +847,6 @@ export class PoliciesServer {
     router.post('/roles/conditions', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityCreatePermission,
       );
 
@@ -888,7 +885,6 @@ export class PoliciesServer {
     router.get('/roles/conditions/:id', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityReadPermission,
       );
 
@@ -926,7 +922,6 @@ export class PoliciesServer {
     router.delete('/roles/conditions/:id', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityDeletePermission,
       );
 
@@ -967,7 +962,6 @@ export class PoliciesServer {
     router.put('/roles/conditions/:id', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityUpdatePermission,
       );
 
@@ -1009,7 +1003,6 @@ export class PoliciesServer {
     router.post('/refresh/:id', async (request, response) => {
       const decision = await this.authorize(
         request,
-        identity,
         policyEntityCreatePermission,
       );
 
