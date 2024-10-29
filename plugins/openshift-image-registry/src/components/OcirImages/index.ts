edef8f7b6df3aaa724b0d6a8f59b98d899d68cb4
Kashish Mittal <kmittal@redhat.com>
2024-10-29 11:41:29 -0400

    deprecate openshift-image-registry
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

--- plugins/openshift-image-registry/src/components/OcirImages/index.ts
+++
@@ -1,1 +0,0 @@
-export { OcirImagesCards } from './OcirImagesCards';
--- plugins/openshift-image-registry/src/plugin.test.ts
+++
@@ -1,7 +0,0 @@
-import { openshiftImageRegistryPlugin } from './plugin';
-
-describe('openshift-image-registry', () => {
-  it('should export plugin', () => {
-    expect(openshiftImageRegistryPlugin).toBeDefined();
-  });
-});
