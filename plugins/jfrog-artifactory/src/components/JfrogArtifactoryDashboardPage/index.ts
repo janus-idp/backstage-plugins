3f46a96eabf8dcea61a0e575b71a9d712d2e4bb2
Kashish Mittal <113269381+04kash@users.noreply.github.com>
2024-11-07 14:15:39 -0500

    chore: removed jfrog-artifactory (#2508)
    * removed jfrog
    
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>
    
    * regen yarn.lock
    
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>
    
    ---------
    
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

--- plugins/jfrog-artifactory/src/components/JfrogArtifactoryDashboardPage/index.ts
+++
@@ -1,1 +0,0 @@
-export { JfrogArtifactoryDashboardPage } from './JfrogArtifactoryDashboardPage';
--- plugins/jfrog-artifactory/src/plugin.test.ts
+++
@@ -1,7 +0,0 @@
-import { jfrogArtifactoryPlugin } from './plugin';
-
-describe('jfrog artifactory', () => {
-  it('should export plugin', () => {
-    expect(jfrogArtifactoryPlugin).toBeDefined();
-  });
-});
