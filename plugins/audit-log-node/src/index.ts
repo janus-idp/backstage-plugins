ccd19463732e16ffaf8eccda5c367dbc1739c748
Kashish Mittal <kmittal@redhat.com>
2025-02-06 08:57:27 -0500

    chore: add audit-log-node back
    Signed-off-by: Kashish Mittal <kmittal@redhat.com>

---
+++ plugins/audit-log-node/src/index.ts
@@ -0,0 +1,13 @@
+/***/
+/**
+ * Node.js library for common audit logging types and functions
+ *
+ * @packageDocumentation
+ */
+
+/**
+ * Default Audit Logger for constructing audit logs
+ * @public
+ */
+export * from './DefaultAuditLogger';
+export * from './types';
